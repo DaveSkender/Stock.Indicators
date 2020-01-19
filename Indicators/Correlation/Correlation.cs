f0f691755532ee0623da942965883684eae4b0e7
Dave Skender <dave.skender@gmail.com>
2020-01-18 20:57:21 -0500

    add Correlation Coefficient

---
+++ Indicators/Correlation/Correlation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Skender.Stock.Indicators
+{
+    public static partial class Indicator
+    {
+        // CORRELATION (e.g. BETA when used with Indices)
+        public static IEnumerable<CorrResult> GetCorrelation(IEnumerable<Quote> historyA, IEnumerable<Quote> historyB, int lookbackPeriod)
+        {
+            // clean quotes
+            historyA = Cleaners.PrepareHistory(historyA);
+            historyB = Cleaners.PrepareHistory(historyB);
+
+            // initialize results
+            List<CorrResult> results = new List<CorrResult>();
+
+
+            // roll through history for interim data
+            foreach (Quote a in historyA)
+            {
+                Quote b = historyB.Where(x => x.Date == a.Date).FirstOrDefault();
+                if (b == null)
+                {
+                    throw new BadHistoryException("Correlation requires matching dates in provided histories.  {0} not found in historyB.");
+                }
+
+                CorrResult result = new CorrResult
+                {
+                    Index = (int)a.Index,
+                    Date = a.Date,
+                    PriceA = a.Close,
+                    PriceB = b.Close
+                    // other values calculated in class properties
+                };
+                results.Add(result);
+            }
+
+            // compute correlation
+            foreach (CorrResult r in results.Where(x => x.Index >= lookbackPeriod))
+            {
+                IEnumerable<CorrResult> period = results.Where(x => x.Index > (r.Index - lookbackPeriod) && x.Index <= r.Index);
+
+                decimal avgA = period.Select(x => x.PriceA).Average();
+                decimal avgB = period.Select(x => x.PriceB).Average();
+                decimal avgA2 = period.Select(x => x.PriceA2).Average();
+                decimal avgB2 = period.Select(x => x.PriceB2).Average();
+                decimal avgAB = period.Select(x => x.PriceAB).Average();
+
+                decimal varianceA = avgA2 - avgA * avgA;
+                decimal varianceB = avgB2 - avgB * avgB;
+                decimal covariance = avgAB - avgA * avgB;
+
+                r.Correlation = covariance / (decimal)Math.Sqrt((double)(varianceA * varianceB));
+            }
+
+            return results;
+        }
+
+    }
+
+}
---
+++ IndicatorsTests/Test.Correlation.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Skender.Stock.Indicators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StockIndicators.Tests
+{
+    [TestClass]
+    public class CorrelationTests : TestBase
+    {
+
+        [TestMethod()]
+        public void GetCorrelationTest()
+        {
+            int lookbackPeriod = 20;
+
+            IEnumerable<CorrResult> results = Indicator.GetCorrelation(history, historyOther, lookbackPeriod);
+
+            // assertions
+
+            // proper quantities
+            // should always be the same number of results as there is history
+            Assert.AreEqual(502, results.Count());
+            Assert.AreEqual(502 - lookbackPeriod + 1, results.Where(x => x.Correlation != null).Count());
+
+            // sample value
+            CorrResult result = results.Where(x => x.Date == DateTime.Parse("12/31/2018")).FirstOrDefault();
+            Assert.AreEqual((decimal)0.8460, Math.Round((decimal)result.Correlation, 4));
+        }
+    }
+}
