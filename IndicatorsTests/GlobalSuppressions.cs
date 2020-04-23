ddc8c533fe61bd2a4b671b0c433f6365efa2c035
Dave Skender <dave.skender@gmail.com>
2020-04-23 17:27:11 -0400

    suppress known requirement warning

--- IndicatorsTests/GlobalSuppressions.cs
+++ IndicatorsTests/GlobalSuppressions.cs
@@ -7,3 +7,4 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Globalization", "CA1305:Specify IFormatProvider", Justification = "Not doing localization.")]
 [assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Parameter is required.", Scope = "member", Target = "~M:StockIndicators.Tests.TestBase.Initialize(Microsoft.VisualStudio.TestTools.UnitTesting.TestContext)")]
+[assembly: SuppressMessage("Usage", "CA1801: Review unused parameters", Justification = "Parameter is required.", Scope = "member", Target = "~M:StockIndicators.Tests.TestBase.Initialize(Microsoft.VisualStudio.TestTools.UnitTesting.TestContext)")]
