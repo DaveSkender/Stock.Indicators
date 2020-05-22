87ddda2e960fbd2c16648d99047c227fa866e57f
Dave Skender <dave.skender@gmail.com>
2020-05-22 01:00:36 -0400

    fix typo

--- Indicators/Ulcer/Ulcer.cs
+++ Indicators/Ulcer/Ulcer.cs
@@ -18,7 +18,7 @@ namespace Skender.Stock.Indicators
             int minHistory = lookbackPeriod;
             if (qtyHistory < minHistory)
             {
-                throw new BadHistoryException("Insufficient history provided for Beta.  " +
+                throw new BadHistoryException("Insufficient history provided for Ulcer Index.  " +
                         string.Format("You provided {0} periods of history when {1} is required.", qtyHistory, minHistory));
             }
 
