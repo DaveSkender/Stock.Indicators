c64b7f5e509d6a1515e3014c21a478e96e9f724d
Dave Skender <dave.skender@gmail.com>
2020-06-28 16:00:03 -0400

    update model, readme

--- Indicators/Sma/Sma.Models.cs
+++ Indicators/Sma/Sma.Models.cs
@@ -3,7 +3,10 @@
 
     public class SmaResult : ResultBase
     {
-        public decimal? Sma { get; set; }
+        public decimal? Sma { get; set; }  // simple moving average
+        public decimal? Mad { get; set; }  // mean absolute deviation
+        public decimal? Mse { get; set; }  // mean square error
+        public decimal? Mape { get; set; } // mean absolute percentage error
     }
 
 }
