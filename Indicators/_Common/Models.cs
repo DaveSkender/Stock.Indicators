fd8b682a741c776c6c1d0071618ce83a542164be
Dave Skender <dave.skender@gmail.com>
2020-07-09 01:04:33 -0400

    change to protected internal

--- Indicators/_Common/Models.cs
+++ Indicators/_Common/Models.cs
@@ -5,7 +5,7 @@ namespace Skender.Stock.Indicators
 
     public class Quote
     {
-        public int? Index { get; internal set; }
+        public int? Index { get; protected internal set; }
         public DateTime Date { get; set; }
         public decimal Open { get; set; }
         public decimal High { get; set; }
@@ -16,8 +16,8 @@ namespace Skender.Stock.Indicators
 
     public class ResultBase
     {
-        public int Index { get; internal set; }
-        public DateTime Date { get; internal set; }
+        public int Index { get; protected internal set; }
+        public DateTime Date { get; protected internal set; }
     }
 
     internal class BasicData
