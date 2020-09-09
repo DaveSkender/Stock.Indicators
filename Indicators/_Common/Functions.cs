1051ea79966282099929c5b268ced81e2d8b33c3
Dave Skender <dave.skender@gmail.com>
2020-09-09 03:35:25 -0400

    refactor bollinger bands

--- Indicators/_Common/Functions.cs
+++ Indicators/_Common/Functions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Skender.Stock.Indicators
@@ -11,20 +10,15 @@ namespace Skender.Stock.Indicators
         {
             // ref: https://stackoverflow.com/questions/2253874/standard-deviation-in-linq
 
-            double ret = 0;
+            double sd = 0;
             int count = values.Length;
             if (count > 1)
             {
-                //Compute the Average
                 double avg = values.Average();
-
-                //Perform the Sum of (value-avg)^2
                 double sum = values.Sum(d => (d - avg) * (d - avg));
-
-                //Put it all together
-                ret = Math.Sqrt(sum / count);
+                sd = Math.Sqrt(sum / count);
             }
-            return ret;
+            return sd;
         }
 
     }
