c738b33bc63352c98b9aff0bf83f3e6f66961929
Dave Skender <dave.skender@gmail.com>
2020-09-11 15:06:32 -0400

    update perf test command line option

--- tests/performance/Program.cs
+++ tests/performance/Program.cs
@@ -9,10 +9,22 @@ namespace Performance.Tests
 {
     public class Program
     {
-        private static void Main()
+        private static void Main(string[] args)
         {
             Console.WriteLine("Running benchmarks");
-            BenchmarkRunner.Run(typeof(Program).Assembly);
+
+            if (args.Length == 0)
+            {
+                // run all
+                // example: dotnet run -c Release
+                BenchmarkRunner.Run(typeof(Program).Assembly);
+            }
+            else
+            {
+                // run based on arguments (e.g. filter)
+                // example: dotnet run -c Release --filter *.GetAdx
+                BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+            }
         }
     }
 
