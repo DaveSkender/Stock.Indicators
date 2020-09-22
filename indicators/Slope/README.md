024012197372c415e84750c87f17b8da1202a8e8
Dave Skender <dave.skender@gmail.com>
2020-09-22 18:30:30 -0400

    add Slope and Linear Regression

---
+++ indicators/Slope/README.md
@@ -0,0 +1,55 @@
+﻿# Slope and Linear Regression
+
+[Slope of the best fit line](https://school.stockcharts.com/doku.php?id=technical_indicators:slope), determined by Linear Regression on Close price.  It can be used to help identify trend strength and direction.  R-Squared (R&sup2;) is also output as a fitness measure of the linear regression; this is not the same as the more prevalent [Correlation (R&sup2;)](../Correlation/README.md).  A best-fit `Line` is also returned for the last lookback segment.
+
+<!-- ![image](chart.png) -->
+
+```csharp
+// usage
+IEnumerable<SlopeResult> results = Indicator.GetSlope(history, lookbackPeriod);  
+```
+
+## Parameters
+
+| name | type | notes
+| -- |-- |--
+| `history` | IEnumerable\<[Quote](../../docs/GUIDE.md#quote)\> | Historical Quotes data should be at any consistent frequency (day, hour, minute, etc).  You must supply at least `N` periods.
+| `lookbackPeriod` | int | Number of periods (`N`) for the linear regression.  Must be greater than 0.
+
+## Response
+
+```csharp
+IEnumerable<SlopeResult>
+```
+
+The first `N-1` periods will have `null` values for `Slope` since there's not enough data to calculate.  We always return the same number of elements as there are in the historical quotes.
+
+### SlopeResult
+
+| name | type | notes
+| -- |-- |--
+| `Date` | DateTime | Date
+| `Slope` | decimal | Slope `b` of the best-fit line of Close price
+| `Intercept` | decimal | Y-Intercept `m` of the best-fit line
+| `StdDev` | double | Standard Deviation of Close price over `N` lookback periods
+| `RSquared` | double | R-Squared (R&sup2;), aka Coefficient of Determination
+| `Line` | decimal | Best-fit line `y` segment for the last 'N' periods (`y=mx+b` where x is period)
+
+## Example
+
+```csharp
+// fetch historical quotes from your favorite feed, in Quote format
+IEnumerable<Quote> historySPX = GetHistoryFromFeed("SPX");
+
+// calculate 20-period Slope
+IEnumerable<SlopeResult> results = Indicator.GetSlope(history,20);
+
+// use results as needed
+DateTime evalDate = DateTime.Parse("12/31/2018");
+SlopeResult result = results.Where(x=>x.Date==evalDate).FirstOrDefault();
+Console.WriteLine("Slope(20) on {0} was {1}", result.Date, result.Slope);
+```
+
+```bash
+Slope(20) on 12/31/2018 was 0.85
+```
---
+++ indicators/Slope/Slope.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Skender.Stock.Indicators
+{
+    public static partial class Indicator
+    {
+        // SLOPE AND LINEAR REGRESSION
+        // ref: https://stattrek.com/regression/linear-regression.aspx
+
+        public static IEnumerable<SlopeResult> GetSlope(IEnumerable<Quote> history, int lookbackPeriod)
+        {
+            // clean quotes
+            List<Quote> historyList = Cleaners.PrepareHistory(history).ToList();
+
+            // validate parameters
+            ValidateSlope(history, lookbackPeriod);
+
+            // initialize
+            List<SlopeResult> results = new List<SlopeResult>();
+
+            // roll through history for interim data
+            for (int i = 0; i < historyList.Count; i++)
+            {
+                Quote h = historyList[i];
+
+                SlopeResult r = new SlopeResult
+                {
+                    Index = (int)h.Index,
+                    Date = h.Date
+                };
+
+                results.Add(r);
+
+                // skip initial lookback period
+                if (h.Index < lookbackPeriod)
+                {
+                    continue;
+                }
+
+                // get averages for period
+                decimal sumX = 0m;
+                decimal sumY = 0m;
+
+                for (int p = r.Index - lookbackPeriod; p < r.Index; p++)
+                {
+                    Quote d = historyList[p];
+
+                    sumX += (decimal)d.Index;
+                    sumY += d.Close;
+                }
+
+                decimal avgX = sumX / lookbackPeriod;
+                decimal avgY = sumY / lookbackPeriod;
+
+                // least squares method (for single X variable)
+                decimal sumSqXY = 0m;
+                decimal sumSqX = 0m;
+                decimal sumSqY = 0;
+
+                for (int p = r.Index - lookbackPeriod; p < r.Index; p++)
+                {
+                    Quote d = historyList[p];
+
+                    decimal devX = ((decimal)d.Index - avgX);
+                    decimal devY = (d.Close - avgY);
+
+                    sumSqX += devX * devX;
+                    sumSqY += devY * devY;
+                    sumSqXY += devX * devY;
+                }
+
+                r.Slope = sumSqXY / sumSqX;
+                r.Intercept = avgY - r.Slope * avgX;
+
+                // calculate R-Squared
+                double stdDevX = Math.Sqrt((double)sumSqX / lookbackPeriod);
+                double stdDevY = Math.Sqrt((double)sumSqY / lookbackPeriod);
+                r.StdDev = stdDevY;
+
+                if (stdDevX * stdDevY != 0)
+                {
+                    double R = ((double)sumSqXY / (stdDevX * stdDevY)) / lookbackPeriod;
+                    r.RSquared = R * R;
+                }
+
+                // add last Line (y = mx + b)
+                if (i + 1 == historyList.Count)
+                {
+                    for (int p = r.Index - lookbackPeriod; p < r.Index; p++)
+                    {
+                        SlopeResult d = results[p];
+                        d.Line = r.Slope * d.Index + r.Intercept;
+                    }
+                }
+            }
+
+            return results;
+        }
+
+
+        private static void ValidateSlope(IEnumerable<Quote> history, int lookbackPeriod)
+        {
+
+            // check parameters
+            if (lookbackPeriod <= 0)
+            {
+                throw new BadParameterException("Lookback period must be greater than 0 for Slope.");
+            }
+
+            // check history
+            int qtyHistory = history.Count();
+            int minHistory = lookbackPeriod;
+            if (qtyHistory < minHistory)
+            {
+                throw new BadHistoryException("Insufficient history provided for Slope.  " +
+                        string.Format(englishCulture,
+                        "You provided {0} periods of history when at least {1} is required.",
+                        qtyHistory, minHistory));
+            }
+        }
+    }
+
+}
