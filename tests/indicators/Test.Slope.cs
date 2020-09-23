24a7fa8bc2b44259d172a84d02933f637a98375e
Dave Skender <dave.skender@gmail.com>
2020-09-23 03:56:50 -0400

    add missing unit test

--- tests/indicators/Test.Slope.cs
+++ tests/indicators/Test.Slope.cs
@@ -22,6 +22,7 @@ namespace Internal.Tests
             // should always be the same number of results as there is history
             Assert.AreEqual(502, results.Count());
             Assert.AreEqual(502 - lookbackPeriod + 1, results.Where(x => x.Slope != null).Count());
+            Assert.AreEqual(502 - lookbackPeriod + 1, results.Where(x => x.StdDev != null).Count());
             Assert.AreEqual(lookbackPeriod, results.Where(x => x.Line != null).Count());
 
             // sample values
@@ -29,18 +30,21 @@ namespace Internal.Tests
             Assert.AreEqual(-1.689143m, Math.Round((decimal)r1.Slope, 6));
             Assert.AreEqual(1083.7629m, Math.Round((decimal)r1.Intercept, 4));
             Assert.AreEqual(0.7955m, Math.Round((decimal)r1.RSquared, 4));
+            Assert.AreEqual(10.9202m, Math.Round((decimal)r1.StdDev, 4));
             Assert.AreEqual(235.8131m, Math.Round((decimal)r1.Line, 4));
 
             SlopeResult r2 = results.Where(x => x.Index == 483).FirstOrDefault();
             Assert.AreEqual(-0.337015m, Math.Round((decimal)r2.Slope, 6));
             Assert.AreEqual(425.1111m, Math.Round((decimal)r2.Intercept, 4));
             Assert.AreEqual(0.1730m, Math.Round((decimal)r2.RSquared, 4));
+            Assert.AreEqual(4.6719m, Math.Round((decimal)r2.StdDev, 4));
             Assert.AreEqual(267.9069m, Math.Round((decimal)r2.Line, 4));
 
             SlopeResult r3 = results.Where(x => x.Index == 250).FirstOrDefault();
             Assert.AreEqual(0.312406m, Math.Round((decimal)r3.Slope, 6));
             Assert.AreEqual(180.4164m, Math.Round((decimal)r3.Intercept, 4));
             Assert.AreEqual(0.8056m, Math.Round((decimal)r3.RSquared, 4));
+            Assert.AreEqual(2.0071m, Math.Round((decimal)r3.StdDev, 4));
             Assert.AreEqual(null, r3.Line);
         }
 
