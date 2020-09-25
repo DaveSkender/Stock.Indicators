96ac22fe8914ad3863dfd7abe99962472adfd088
Chetan Yanamandra <ychetankumarsarma@gmail.com>
2020-09-25 14:40:50 -0400

    adding ultimate-oscillator indicator

---
+++ indicators/Ultimate/README.md
@@ -0,0 +1,53 @@
+﻿# Ultimate Oscillator
+
+[Ultimate Oscillator](https://en.wikipedia.org/wiki/Ultimate_oscillator) uses several lookback periods to weigh buying power against true range price to produce on oversold / overbought oscillator.
+
+![image](chart.png)
+
+```csharp
+// usage
+IEnumerable<UltimateResult> results = Indicator.GetUltimate(history, shortPeriod, middlePeriod, longPeriod);  
+```
+
+## Parameters
+
+| name | type | notes
+| -- |-- |--
+| `history` | IEnumerable\<[Quote](../../docs/GUIDE.md#quote)\> | Historical Quotes data should be at any consistent frequency (day, hour, minute, etc).  You must supply at least `L+1` periods of `history`.
+| `shortPeriod` | int | Number of periods (`S`) in the short lookback.  Must be greater than 0.
+| `middlePeriod` | int | Number of periods (`M`) in the middle lookback.  Must be greater than `S`.
+| `longPeriod` | int | Number of periods (`L`) in the long lookback.  Must be greater than `M`.
+
+## Response
+
+```csharp
+IEnumerable<UltimateResult>
+```
+
+The first `L-1` periods will have `null` Ultimate values since there's not enough data to calculate.  We always return the same number of elements as there are in the historical quotes.
+
+### UltimateResult
+
+| name | type | notes
+| -- |-- |--
+| `Date` | DateTime | Date
+| `Ultimate` | decimal | Simple moving average for `N` lookback periods
+
+## Example
+
+```csharp
+// fetch historical quotes from your favorite feed, in Quote format
+IEnumerable<Quote> history = GetHistoryFromFeed("MSFT");
+
+// calculate 20-period Ultimate
+IEnumerable<UltimateResult> results = Indicator.GetUltimate(history,7,14,28);
+
+// use results as needed
+DateTime evalDate = DateTime.Parse("12/31/2018");
+UltimateResult result = results.Where(x=>x.Date==evalDate).FirstOrDefault();
+Console.WriteLine("ULT on {0} was {1}", result.Date, result.Ultimate);
+```
+
+```bash
+ULT on 12/31/2018 was 49.53
+```
---
+++ tests/indicators/Test.Ultimate.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Skender.Stock.Indicators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Internal.Tests
+{
+    [TestClass]
+    public class UltimateTests : TestBase
+    {
+
+        [TestMethod()]
+        public void GetUltimateTest()
+        {
+            IEnumerable<UltimateResult> results = Indicator.GetUltimate(history, 7, 14, 28);
+
+            // assertions
+
+            // proper quantities
+            // should always be the same number of results as there is history
+            Assert.AreEqual(502, results.Count());
+            Assert.AreEqual(474, results.Where(x => x.Ultimate != null).Count());
+
+            // sample values
+            UltimateResult r1 = results.Where(x => x.Index == 502).FirstOrDefault();
+            Assert.AreEqual(49.5257m, Math.Round((decimal)r1.Ultimate, 4));
+
+            UltimateResult r2 = results.Where(x => x.Index == 250).FirstOrDefault();
+            Assert.AreEqual(45.3121m, Math.Round((decimal)r2.Ultimate, 4));
+
+            UltimateResult r3 = results.Where(x => x.Index == 75).FirstOrDefault();
+            Assert.AreEqual(51.7770m, Math.Round((decimal)r3.Ultimate, 4));
+
+        }
+
+
+        /* EXCEPTIONS */
+
+        [TestMethod()]
+        [ExpectedException(typeof(BadParameterException), "Bad short period.")]
+        public void BadShortPeriod()
+        {
+            Indicator.GetUltimate(history, 0);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(BadParameterException), "Bad middle period.")]
+        public void BadMiddlePeriod()
+        {
+            Indicator.GetUltimate(history, 7, 6);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(BadParameterException), "Bad long period.")]
+        public void BadLongPeriod()
+        {
+            Indicator.GetUltimate(history, 7, 14, 11);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(BadHistoryException), "Insufficient history.")]
+        public void InsufficientHistory()
+        {
+            Indicator.GetUltimate(history.Where(x => x.Index <= 28), 7, 14, 28);
+        }
+    }
+}
