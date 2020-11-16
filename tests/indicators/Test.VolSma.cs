a85d28843c076eb5421122f5427785e15008d6aa
Dave Skender <dave.skender@gmail.com>
2020-11-16 00:08:00 -0500

    volume sma

--- tests/indicators/Test.VolSma.cs
+++ tests/indicators/Test.VolSma.cs
@@ -14,23 +14,23 @@ namespace Internal.Tests
         public void GetVolSmaTest()
         {
             int lookbackPeriod = 20;
-            IEnumerable<VolSmaResult> results = Indicator.GetVolSma(history, lookbackPeriod);
+            List<VolSmaResult> results = Indicator.GetVolSma(history, lookbackPeriod).ToList();
 
             // assertions
 
             // proper quantities
             // should always be the same number of results as there is history
-            Assert.AreEqual(502, results.Count());
+            Assert.AreEqual(502, results.Count);
             Assert.AreEqual(483, results.Where(x => x.VolSma != null).Count());
 
             // sample values
-            VolSmaResult r1 = results.Where(x => x.Index == 25).FirstOrDefault();
+            VolSmaResult r1 = results[24];
             Assert.AreEqual(77293768.2m, r1.VolSma);
 
-            VolSmaResult r2 = results.Where(x => x.Index == 291).FirstOrDefault();
+            VolSmaResult r2 = results[290];
             Assert.AreEqual(157958070.8m, r2.VolSma);
 
-            VolSmaResult r3 = results.Where(x => x.Index == 502).FirstOrDefault();
+            VolSmaResult r3 = results[501];
             Assert.AreEqual(DateTime.ParseExact("12/31/2018", "MM/dd/yyyy", englishCulture), r3.Date);
             Assert.AreEqual(147031456m, r3.Volume);
             Assert.AreEqual(163695200m, r3.VolSma);
