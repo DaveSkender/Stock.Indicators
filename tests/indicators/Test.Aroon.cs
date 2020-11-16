2c58927db13b36c0c473d1af685ef65c4def03f3
Dave Skender <dave.skender@gmail.com>
2020-11-15 20:27:34 -0500

    aroon

--- tests/indicators/Test.Aroon.cs
+++ tests/indicators/Test.Aroon.cs
@@ -14,39 +14,39 @@ namespace Internal.Tests
         public void GetAroonTest()
         {
             int lookbackPeriod = 25;
-            IEnumerable<AroonResult> results = Indicator.GetAroon(history, lookbackPeriod);
+            List<AroonResult> results = Indicator.GetAroon(history, lookbackPeriod).ToList();
 
             // assertions
 
             // proper quantities
             // should always be the same number of results as there is history
-            Assert.AreEqual(502, results.Count());
+            Assert.AreEqual(502, results.Count);
             Assert.AreEqual(477, results.Where(x => x.AroonUp != null).Count());
             Assert.AreEqual(477, results.Where(x => x.AroonDown != null).Count());
             Assert.AreEqual(477, results.Where(x => x.Oscillator != null).Count());
 
             // sample values
-            AroonResult r1 = results.Where(x => x.Index == 502).FirstOrDefault();
+            AroonResult r1 = results[501];
             Assert.AreEqual(28m, r1.AroonUp);
             Assert.AreEqual(88m, r1.AroonDown);
             Assert.AreEqual(-60m, r1.Oscillator);
 
-            AroonResult r2 = results.Where(x => x.Index == 459).FirstOrDefault();
+            AroonResult r2 = results[458];
             Assert.AreEqual(0m, r2.AroonUp);
             Assert.AreEqual(100m, r2.AroonDown);
             Assert.AreEqual(-100m, r2.Oscillator);
 
-            AroonResult r3 = results.Where(x => x.Index == 299).FirstOrDefault();
+            AroonResult r3 = results[298];
             Assert.AreEqual(0m, r3.AroonUp);
             Assert.AreEqual(20m, r3.AroonDown);
             Assert.AreEqual(-20m, r3.Oscillator);
 
-            AroonResult r4 = results.Where(x => x.Index == 294).FirstOrDefault();
+            AroonResult r4 = results[293];
             Assert.AreEqual(0m, r4.AroonUp);
             Assert.AreEqual(40m, r4.AroonDown);
             Assert.AreEqual(-40m, r4.Oscillator);
 
-            AroonResult r5 = results.Where(x => x.Index == 211).FirstOrDefault();
+            AroonResult r5 = results[210];
             Assert.AreEqual(100m, r5.AroonUp);
             Assert.AreEqual(0m, r5.AroonDown);
             Assert.AreEqual(100m, r5.Oscillator);
