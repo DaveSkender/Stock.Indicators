3a2624fb5370f0a93132db45fb919dc182ea217c
Dave Skender <dave.skender@gmail.com>
2020-11-15 22:06:36 -0500

    macd

--- tests/indicators/Test.Macd.cs
+++ tests/indicators/Test.Macd.cs
@@ -17,29 +17,31 @@ namespace Internal.Tests
             int slowPeriod = 26;
             int signalPeriod = 9;
 
-            IEnumerable<MacdResult> results = Indicator.GetMacd(history, fastPeriod, slowPeriod, signalPeriod);
+            List<MacdResult> results =
+                Indicator.GetMacd(history, fastPeriod, slowPeriod, signalPeriod)
+                .ToList();
 
             // assertions
 
             // proper quantities
             // should always be the same number of results as there is history
-            Assert.AreEqual(502, results.Count());
+            Assert.AreEqual(502, results.Count);
             Assert.AreEqual(477, results.Where(x => x.Macd != null).Count());
             Assert.AreEqual(469, results.Where(x => x.Signal != null).Count());
             Assert.AreEqual(469, results.Where(x => x.Histogram != null).Count());
 
             // sample values
-            MacdResult r1 = results.Where(x => x.Index == 502).FirstOrDefault();
+            MacdResult r1 = results[501];
             Assert.AreEqual(-6.2198m, Math.Round((decimal)r1.Macd, 4));
             Assert.AreEqual(-5.8569m, Math.Round((decimal)r1.Signal, 4));
             Assert.AreEqual(-0.3629m, Math.Round((decimal)r1.Histogram, 4));
 
-            MacdResult r2 = results.Where(x => x.Index == 50).FirstOrDefault();
+            MacdResult r2 = results[49];
             Assert.AreEqual(1.7203m, Math.Round((decimal)r2.Macd, 4));
             Assert.AreEqual(1.9675m, Math.Round((decimal)r2.Signal, 4));
             Assert.AreEqual(-0.2472m, Math.Round((decimal)r2.Histogram, 4));
 
-            MacdResult r3 = results.Where(x => x.Index == 250).FirstOrDefault();
+            MacdResult r3 = results[249];
             Assert.AreEqual(2.2353m, Math.Round((decimal)r3.Macd, 4));
             Assert.AreEqual(2.3141m, Math.Round((decimal)r3.Signal, 4));
             Assert.AreEqual(-0.0789m, Math.Round((decimal)r3.Histogram, 4));
