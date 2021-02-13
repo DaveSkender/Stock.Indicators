28d5c9f8da13eca74ea3488bacd4c9d56f57233d
Ibi Keller <ibi.keller@gmail.com>
2021-02-12 19:38:52 -0500

    add Choppiness Index (#359)
    * add Choppiness Index

---
+++ indicators/Chop/Chop.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Skender.Stock.Indicators
+{
+    public static partial class Indicator
+    {
+        // CHOPPINESS INDEX
+        /// <include file='./info.xml' path='indicator/*' />
+        ///
+        public static IEnumerable<ChopResult> GetChop<TQuote>(
+            IEnumerable<TQuote> history,
+            int lookbackPeriod = 14)
+            where TQuote : IQuote
+        {
+
+            // sort history
+            List<TQuote> historyList = history.Sort();
+
+            // check parameter arguments
+            ValidateChop(historyList, lookbackPeriod);
+
+            // initialize
+            decimal sum;
+            decimal high;
+            decimal low;
+            decimal range;
+
+            int size = historyList.Count;
+            List<ChopResult> results = new List<ChopResult>(size);
+            decimal[] trueHigh = new decimal[size];
+            decimal[] trueLow = new decimal[size];
+            decimal[] trueRange = new decimal[size];
+
+            // roll through history
+            for (int i = 0; i < historyList.Count; i++)
+            {
+                ChopResult r = new ChopResult
+                {
+                    Date = historyList[i].Date
+                };
+                results.Add(r);
+
+                if (i > 0)
+                {
+                    trueHigh[i] = Math.Max(historyList[i].High, historyList[i - 1].Close);
+                    trueLow[i] = Math.Min(historyList[i].Low, historyList[i - 1].Close);
+                    trueRange[i] = trueHigh[i] - trueLow[i];
+
+                    // calculate CHOP
+
+                    if (i >= lookbackPeriod)
+                    {
+                        // reset measurements
+                        sum = trueRange[i];
+                        high = trueHigh[i];
+                        low = trueLow[i];
+
+                        // iterate over lookback window
+                        for (int j = 1; j < lookbackPeriod; j++)
+                        {
+                            sum += trueRange[i - j];
+                            high = Math.Max(high, trueHigh[i - j]);
+                            low = Math.Min(low, trueLow[i - j]);
+                        }
+
+                        range = high - low;
+
+                        // calculate CHOP
+                        if (range != 0)
+                        {
+                            r.Chop = (decimal)(100 * (Math.Log((double)(sum / range)) / Math.Log(lookbackPeriod)));
+                        }
+                    }
+                }
+            }
+            return results;
+        }
+
+        private static void ValidateChop<TQuote>(
+            List<TQuote> history,
+            int lookbackPeriod)
+            where TQuote : IQuote
+
+        {
+            // check parameter arguments
+            if (lookbackPeriod <= 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(lookbackPeriod), lookbackPeriod,
+                    "Lookback period must be greater than 1 for CHOP.");
+            }
+
+            // check history
+            int qtyHistory = history.Count;
+            int minHistory = lookbackPeriod + 1;
+            if (qtyHistory < minHistory)
+            {
+                string message = "Insufficient history provided for CHOP.  " +
+                    string.Format(EnglishCulture,
+                    "You provided {0} periods of history when at least {1} is required.",
+                    qtyHistory, minHistory);
+
+                throw new BadHistoryException(nameof(history), message);
+            }
+        }
+    }
+}
---
+++ tests/indicators/Test.Chop.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Skender.Stock.Indicators;
+
+namespace Internal.Tests
+{
+    [TestClass]
+    public class Chop : TestBase
+    {
+        [TestMethod]
+        public void Standard()
+        {
+            int lookbackPeriod = 14;
+
+            List<ChopResult> results = Indicator.GetChop(history, lookbackPeriod)
+                .ToList();
+
+            // assertions
+
+            // proper quantities
+            // should always be the same number of results as there is history
+            Assert.AreEqual(502, results.Count);
+            Assert.AreEqual(488, results.Where(x => x.Chop != null).Count());
+
+            // sample values
+            ChopResult r1 = results[13];
+            Assert.AreEqual(null, r1.Chop);
+
+            ChopResult r2 = results[14];
+            Assert.AreEqual(69.9967m, Math.Round((decimal)r2.Chop, 4));
+
+            ChopResult r3 = results[249];
+            Assert.AreEqual(41.8499m, Math.Round((decimal)r3.Chop, 4));
+
+            ChopResult r4 = results[501];
+            Assert.AreEqual(38.6526m, Math.Round((decimal)r4.Chop, 4));
+        }
+
+        [TestMethod]
+        public void SmallLookback()
+        {
+            int lookbackPeriod = 2;
+            List<ChopResult> results = Indicator.GetChop(history, lookbackPeriod).ToList();
+
+            // assertions
+
+            // proper quantities
+            // should always be the same number of results as there is history
+            Assert.AreEqual(502, results.Count);
+            Assert.AreEqual(500, results.Where(x => x.Chop != null).Count());
+        }
+
+        [TestMethod]
+        public void BadData()
+        {
+            IEnumerable<ChopResult> r = Indicator.GetChop(historyBad, 20);
+            Assert.AreEqual(502, r.Count());
+        }
+
+        [TestMethod]
+        public void Exceptions()
+        {
+            // bad lookback period
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+                Indicator.GetChop(history, 1));
+
+            // insufficient history
+            Assert.ThrowsException<BadHistoryException>(() =>
+                Indicator.GetChop(HistoryTestData.Get(30), 30));
+        }
+    }
+}
