046ff6f7cbaed02dcf56bb6c0dc18f83cb107a3a
Dave Skender <dave.skender@gmail.com>
2021-02-27 14:20:14 -0500

    - add convergence test - fix null error in tests

--- tests/indicators/Test.Smma.cs
+++ tests/indicators/Test.Smma.cs
@@ -22,18 +22,32 @@ namespace Internal.Tests
             // proper quantities
             // should always be the same number of results as there is history
             Assert.AreEqual(502, results.Count);
-            Assert.AreEqual(502 - lookbackPeriod + 1, results.Where(x => x.Smma != null).Count());
+            Assert.AreEqual(483, results.Where(x => x.Smma != null).Count());
 
             // starting calculations at proper index
-            Assert.IsNull(results[18].Smma.Value);
-            Assert.IsNotNull(results[19].Smma.Value);
+            Assert.IsNull(results[18].Smma);
+            Assert.IsNotNull(results[19].Smma);
 
-            // sample value
+            // sample values
             Assert.AreEqual(214.52500m, Math.Round(results[19].Smma.Value, 5));
             Assert.AreEqual(214.55125m, Math.Round(results[20].Smma.Value, 5));
             Assert.AreEqual(214.58319m, Math.Round(results[21].Smma.Value, 5));
             Assert.AreEqual(225.78071m, Math.Round(results[100].Smma.Value, 5));
             Assert.AreEqual(255.67462m, Math.Round(results[501].Smma.Value, 5));
+        }
+
+        [TestMethod]
+        public void Convergence()
+        {
+            foreach (int qty in convergeQuantities)
+            {
+                IEnumerable<Quote> h = HistoryTestData.GetLong(110 + qty);
+                IEnumerable<SmmaResult> r = Indicator.GetSmma(h, 15);
+
+                SmmaResult l = r.LastOrDefault();
+                Console.WriteLine("SMMA(15) on {0:d} with {1,4} periods: {2:N8}",
+                    l.Date, h.Count(), l.Smma);
+            }
         }
 
         [TestMethod]
