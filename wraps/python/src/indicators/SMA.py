c96788578d070e9de54c3af327755757044510b6
LeeDongGeon <secmatth1996@gmail.com>
2021-03-27 04:09:37 +0900

    feat: add wrapper for SMA indicator implements get_sma(), get_sma_extended(), SmaResult class, SmaExtendedResult class
    Signed-off-by: LeeDongGeon <secmatth1996@gmail.com>

---
+++ wraps/python/src/indicators/SMA.py
@@ -0,0 +1,37 @@
+from Skender.Stock.Indicators import Indicator
+from cstypes import List, to_pyfloat
+from .Qoute import Qoute
+
+def get_sma(history, lookbackPeriod: int):
+    sma_list = Indicator.GetSma[Qoute](List(Qoute, history), lookbackPeriod)
+    sma_list = [ SmaResult(r) for r in sma_list ]
+
+    return sma_list
+
+def get_sma_extended(history, lookbackPeriod: int):
+    sma_extended_list = Indicator.GetSmaExtended[Qoute](List(Qoute, history), lookbackPeriod)
+    sma_extended_list = [ SmaExtendedResult(r) for r in sma_extended_list ]
+
+    return sma_extended_list
+
+class SmaResult:
+    def __init__(self,sma_result):
+        self.Date = sma_result.Date
+        self.Sma = to_pyfloat(sma_result.Sma)
+
+    def __str__(self):
+        return str(self.Date) + ": " + str(self.Sma)
+
+class SmaExtendedResult(SmaResult):
+    def __init__(self, sma_extended_result):
+        super().__init__(sma_extended_result)
+        self.Mad = to_pyfloat(sma_extended_result.Mad)
+        self.Mse = to_pyfloat(sma_extended_result.Mse)
+        self.Mape = to_pyfloat(sma_extended_result.Mape)
+
+    def __str__(self):
+        return str(self.Date) + ": " + str(self.Sma) + "\t"\
+             + str(self.Mad) + "\t" + str(self.Mse) + "\t"\
+             + str(self.Mape)
+    
+
--- wraps/python/src/indicators/__init__.py
+++ wraps/python/src/indicators/__init__.py
@@ -2,3 +2,9 @@ import clr
 clr.AddReference(r'dll/Skender.Stock.Indicators')
 
 from .Qoute import (Qoute)
+from .SMA import (
+    get_sma,
+    get_sma_extended,
+    SmaResult,
+    SmaExtendedResult
+)
