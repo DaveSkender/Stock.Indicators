4a0afeac86cfe4cdb7d4c226493635d60970931a
LeeDongGeon <secmatth1996@gmail.com>
2021-03-28 18:47:12 +0900

    fix: set default value of Quote to None
    Signed-off-by: LeeDongGeon <secmatth1996@gmail.com>

--- wraps/python/src/indicators/Quote.py
+++ wraps/python/src/indicators/Quote.py
@@ -2,7 +2,7 @@ from Skender.Stock.Indicators import Quote as CsQuote
 from cstypes import DateTime, Decimal
 
 class Quote(CsQuote):
-    def __init__(self, date, open = 0, high = 0, low = 0, close = 0, volume = 0):
+    def __init__(self, date, open = None, high = None, low = None, close = None, volume = None):
         self.Date = DateTime(date)
         self.Open = Decimal(open)
         self.High = Decimal(high)
