e6cb731c0597090d4b899fc1599e29ca6478f3a0
DongGeon Lee <secmatth1996@gmail.com>
2021-04-13 04:37:21 +0900

    fix: Py Quote to be null-acceptable (#420)

--- wraps/python/SkenderStockIndicators/indicators/common/Quote.py
+++ wraps/python/SkenderStockIndicators/indicators/common/Quote.py
@@ -4,8 +4,8 @@ from SkenderStockIndicators._cstypes import DateTime, Decimal
 class Quote(CsQuote):
     def __init__(self, date, open = None, high = None, low = None, close = None, volume = None):
         self.Date = DateTime(date)
-        self.Open = Decimal(open)
-        self.High = Decimal(high)
-        self.Low = Decimal(low)
-        self.Close = Decimal(close)
-        self.Volume = Decimal(volume)
+        self.Open = Decimal(open) if open else super().Open
+        self.High = Decimal(high) if high else super().High
+        self.Low = Decimal(low) if low else super().Low
+        self.Close = Decimal(close) if close else super().Close
+        self.Volume = Decimal(volume) if volume else super().Volume
