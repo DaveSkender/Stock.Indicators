6c57b2c566005a11557bb16584e9836e21fdbd46
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2021-05-22 19:44:27 -0400

    add Klinger Oscillator (#447)
    +semver: minor

---
+++ indicators/Kvo/Kvo.Models.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Skender.Stock.Indicators
+{
+    [Serializable]
+    public class KvoResult : ResultBase
+    {
+        public decimal? Oscillator { get; set; }
+        public decimal? Signal { get; set; }
+    }
+}
