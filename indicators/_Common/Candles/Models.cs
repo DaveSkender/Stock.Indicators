e5e436251295af8ea4c94b6985af5d75d892d99d
Dave Skender <dave.skender@gmail.com>
2021-08-08 20:46:33 -0400

    rename realbody

--- indicators/_Common/Candles/Models.cs
+++ indicators/_Common/Candles/Models.cs
@@ -9,12 +9,12 @@ namespace Skender.Stock.Indicators
     {
         // raw sizes
         internal decimal Size => High - Low;
-        internal decimal RealBody => (Open > Close) ? (Open - Close) : (Close - Open);
+        internal decimal Body => (Open > Close) ? (Open - Close) : (Close - Open);
         internal decimal UpperWick => High - (Open > Close ? Open : Close);
         internal decimal LowerWick => (Open > Close ? Close : Open) - Low;
 
         // percent sizes
-        internal decimal RealBodyPct => (Size != 0) ? RealBody / Size : 1m;
+        internal decimal BodyPct => (Size != 0) ? Body / Size : 1m;
         internal decimal UpperWickPct => (Size != 0) ? UpperWick / Size : 1m;
         internal decimal LowerWickPct => (Size != 0) ? LowerWick / Size : 1m;
 
--- tests/indicators/Patterns/Test.Candles.cs
+++ tests/indicators/Patterns/Test.Candles.cs
@@ -51,10 +51,10 @@ namespace Internal.Tests
             Candle r0 = results[0];
             Assert.AreEqual(212.8m, r0.Close);
             Assert.AreEqual(1.83m, r0.Size);
-            Assert.AreEqual(0.19m, r0.RealBody);
+            Assert.AreEqual(0.19m, r0.Body);
             Assert.AreEqual(0.55m, r0.UpperWick);
             Assert.AreEqual(1.09m, r0.LowerWick);
-            Assert.AreEqual(0.10383m, Math.Round(r0.RealBodyPct, 5));
+            Assert.AreEqual(0.10383m, Math.Round(r0.BodyPct, 5));
             Assert.AreEqual(0.30055m, Math.Round(r0.UpperWickPct, 5));
             Assert.AreEqual(0.59563m, Math.Round(r0.LowerWickPct, 5));
             Assert.IsTrue(r0.IsBullish);
@@ -62,10 +62,10 @@ namespace Internal.Tests
 
             Candle r351 = results[351];
             Assert.AreEqual(1.24m, r351.Size);
-            Assert.AreEqual(0m, r351.RealBody);
+            Assert.AreEqual(0m, r351.Body);
             Assert.AreEqual(0.69m, r351.UpperWick);
             Assert.AreEqual(0.55m, r351.LowerWick);
-            Assert.AreEqual(0m, Math.Round(r351.RealBodyPct, 5));
+            Assert.AreEqual(0m, Math.Round(r351.BodyPct, 5));
             Assert.AreEqual(0.55645m, Math.Round(r351.UpperWickPct, 5));
             Assert.AreEqual(0.44355m, Math.Round(r351.LowerWickPct, 5));
             Assert.IsFalse(r351.IsBullish);
@@ -73,10 +73,10 @@ namespace Internal.Tests
 
             Candle r501 = results[501];
             Assert.AreEqual(2.67m, r501.Size);
-            Assert.AreEqual(0.36m, r501.RealBody);
+            Assert.AreEqual(0.36m, r501.Body);
             Assert.AreEqual(0.26m, r501.UpperWick);
             Assert.AreEqual(2.05m, r501.LowerWick);
-            Assert.AreEqual(0.13483m, Math.Round(r501.RealBodyPct, 5));
+            Assert.AreEqual(0.13483m, Math.Round(r501.BodyPct, 5));
             Assert.AreEqual(0.09738m, Math.Round(r501.UpperWickPct, 5));
             Assert.AreEqual(0.76779m, Math.Round(r501.LowerWickPct, 5));
             Assert.IsTrue(r501.IsBullish);
