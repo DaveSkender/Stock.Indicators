6b9f5afa1aa73fa8ea348c13c1d1ffbcddf6753f
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2021-09-10 17:26:51 -0400

    refactor: test data (#553)
    * refactor DPO as a sample case

---
+++ tests/indicators/a-d/Dpo/Dpo.Tests.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Skender.Stock.Indicators;
+
+namespace Internal.Tests
+{
+    [TestClass]
+    public class Dpo : TestBase
+    {
+        [TestMethod]
+        public void Standard()
+        {
+            // get expected data
+            List<Quote> qot = new();
+            List<DpoResult> exp = new();
+
+            List<string> csvData = File.ReadAllLines("a-d/Dpo/Dpo.Data.csv")
+                .Skip(1)
+                .ToList();
+
+            for (int i = 0; i < csvData.Count; i++)
+            {
+                string[] csv = csvData[i].Split(",");
+                DateTime date = Convert.ToDateTime(csv[1], EnglishCulture);
+
+                qot.Add(new Quote
+                {
+                    Date = date,
+                    Close = csv[5].ToDecimal()
+                });
+
+                exp.Add(new DpoResult
+                {
+                    Date = date,
+                    Sma = csv[6].ToDecimalNull(),
+                    Dpo = csv[7].ToDecimalNull()
+                });
+            }
+
+            // calculate actual data
+            List<DpoResult> act = qot.GetDpo(14)
+                .ToList();
+
+            // assertions
+            Assert.AreEqual(exp.Count, act.Count);
+
+            // compare all values
+            for (int i = 0; i < exp.Count; i++)
+            {
+                DpoResult e = exp[i];
+                DpoResult a = act[i];
+
+                Assert.AreEqual(e.Date, a.Date);
+                Assert.AreEqual(e.Sma, a.Sma.NullRound(5), $"at index {i}");
+                Assert.AreEqual(e.Dpo, a.Dpo.NullRound(5), $"at index {i}");
+            }
+        }
+
+        [TestMethod]
+        public void ConvertToQuotes()
+        {
+            List<Quote> newQuotes = quotes.GetDpo(14)
+                .ConvertToQuotes()
+                .ToList();
+
+            Assert.AreEqual(489, newQuotes.Count);
+
+            Quote q = newQuotes.LastOrDefault();
+            Assert.AreEqual(2.18214m, Math.Round(q.Close, 5));
+        }
+
+        [TestMethod]
+        public void BadData()
+        {
+            IEnumerable<DpoResult> r = badQuotes.GetDpo(5);
+            Assert.AreEqual(502, r.Count());
+        }
+
+        [TestMethod]
+        public void Exceptions()
+        {
+            // bad SMA period
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+                quotes.GetDpo(0));
+
+            // insufficient quotes
+            Assert.ThrowsException<BadQuotesException>(() =>
+                Indicator.GetDpo(TestData.GetDefault(10), 11));
+        }
+    }
+}
---
+++ tests/indicators/s-z/Vwap/Vwap.Tests.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Skender.Stock.Indicators;
+
+namespace Internal.Tests
+{
+    [TestClass]
+    public class Vwap : TestBase
+    {
+        private readonly IEnumerable<Quote> intraday = TestData.GetIntraday()
+            .OrderBy(x => x.Date)
+            .Take(391);
+
+        [TestMethod]
+        public void Standard()
+        {
+
+            List<VwapResult> results = intraday.GetVwap()
+                .ToList();
+
+            // assertions
+
+            // should always be the same number of results as there is quotes
+            Assert.AreEqual(391, results.Count);
+            Assert.AreEqual(391, results.Where(x => x.Vwap != null).Count());
+
+            // sample values
+            VwapResult r1 = results[0];
+            Assert.AreEqual(367.4800m, Math.Round((decimal)r1.Vwap, 4));
+
+            VwapResult r2 = results[1];
+            Assert.AreEqual(367.4223m, Math.Round((decimal)r2.Vwap, 4));
+
+            VwapResult r3 = results[369];
+            Assert.AreEqual(367.9494m, Math.Round((decimal)r3.Vwap, 4));
+
+            VwapResult r4 = results[390];
+            Assert.AreEqual(368.1804m, Math.Round((decimal)r4.Vwap, 4));
+        }
+
+        [TestMethod]
+        public void WithStartDate()
+        {
+            DateTime startDate =
+                DateTime.ParseExact("2020-12-15 10:00", "yyyy-MM-dd h:mm", EnglishCulture);
+
+            List<VwapResult> results = Indicator.GetVwap(intraday, startDate)
+                .ToList();
+
+            // assertions
+
+            // should always be the same number of results as there is quotes
+            Assert.AreEqual(391, results.Count);
+            Assert.AreEqual(361, results.Where(x => x.Vwap != null).Count());
+
+            // sample values
+            VwapResult r1 = results[29];
+            Assert.AreEqual(null, r1.Vwap);
+
+            VwapResult r2 = results[30];
+            Assert.AreEqual(366.8100m, Math.Round((decimal)r2.Vwap, 4));
+
+            VwapResult r3 = results[369];
+            Assert.AreEqual(368.0511m, Math.Round((decimal)r3.Vwap, 4));
+
+            VwapResult r4 = results[390];
+            Assert.AreEqual(368.2908m, Math.Round((decimal)r4.Vwap, 4));
+        }
+
+        [TestMethod]
+        public void BadData()
+        {
+            IEnumerable<VwapResult> r = Indicator.GetVwap(badQuotes);
+            Assert.AreEqual(502, r.Count());
+        }
+
+        [TestMethod]
+        public void Removed()
+        {
+            // no start date
+            List<VwapResult> results = intraday.GetVwap()
+                .RemoveWarmupPeriods()
+                .ToList();
+
+            // assertions
+            Assert.AreEqual(391, results.Count);
+
+            VwapResult last = results.LastOrDefault();
+            Assert.AreEqual(368.1804m, Math.Round((decimal)last.Vwap, 4));
+
+            // with start date
+            DateTime startDate =
+            DateTime.ParseExact("2020-12-15 10:00", "yyyy-MM-dd h:mm", EnglishCulture);
+
+            List<VwapResult> sdResults = Indicator.GetVwap(intraday, startDate)
+                .RemoveWarmupPeriods()
+                .ToList();
+
+            // assertions
+            Assert.AreEqual(361, sdResults.Count);
+
+            VwapResult sdLast = sdResults.LastOrDefault();
+            Assert.AreEqual(368.2908m, Math.Round((decimal)sdLast.Vwap, 4));
+        }
+
+        [TestMethod]
+        public void Exceptions()
+        {
+            // bad SMA period
+            DateTime startDate =
+                DateTime.ParseExact("2000-12-15", "yyyy-MM-dd", EnglishCulture);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+                Indicator.GetVwap(quotes, startDate));
+
+            // insufficient quotes
+            Assert.ThrowsException<BadQuotesException>(() =>
+                Indicator.GetVwap(TestData.GetDefault(0)));
+        }
+    }
+}
