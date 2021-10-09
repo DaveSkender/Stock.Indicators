1582dca1d4f87769c6b1550b1dcc4fdba0553766
Dave Skender <dave.skender@gmail.com>
2021-10-09 13:50:48 -0400

    update example

--- examples/ConsoleApp/Program.cs
+++ examples/ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using Newtonsoft.Json;
@@ -11,24 +12,33 @@ namespace ConsoleApp
     {
         public static void Main()
         {
-            // Fetch historical quotes from data provider
+
+            // fetch historical quotes from data provider
             IEnumerable<Quote> quotes = GetHistoryFromFeed();
 
-
-            // Calculate 10-period SMA
+            // calculate 10-period SMA
             IEnumerable<SmaResult> results = quotes.GetSma(10);
 
-            // Show results
+            // show results
             Console.WriteLine("SMA Results ---------------------------");
 
-            // --> only showing last 10 records for brevity
             foreach (SmaResult r in results.TakeLast(10))
+            // only showing last 10 records for brevity
             {
                 Console.WriteLine($"SMA on {r.Date:u} was ${r.Sma:N3}");
             }
 
+            // optionally, you can lookup individual values by date
+            DateTime lookupDate = DateTime
+                .Parse("2021-08-12T17:08:17.9746795+02:00", CultureInfo.InvariantCulture);
 
-            // Analyze results (compare to quote values)
+            decimal? specificSma = results.Find(lookupDate).Sma;
+
+            Console.WriteLine();
+            Console.WriteLine("SMA on Specific Date ------------------");
+            Console.WriteLine($"SMA on {lookupDate:u} was ${specificSma:N3}");
+
+            // analyze results (compare to quote values)
             Console.WriteLine();
             Console.WriteLine("SMA Analysis --------------------------");
 
