3127eaaec0bd2726759543bdc82c4206032ddad2
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2022-01-23 01:03:39 -0500

    chore: update unit tests (#687)

--- src/s-z/Vwma/Vwma.Models.cs
+++ src/s-z/Vwma/Vwma.Models.cs
@@ -3,6 +3,5 @@ namespace Skender.Stock.Indicators;
 [Serializable]
 public class VwmaResult : ResultBase
 {
-    public decimal Volume { get; set; } // for reference only
     public decimal? Vwma { get; set; } // simple moving average of volume
 }
