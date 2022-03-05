d6750183d82fead2747810555b6bfe3bc83fba79
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2022-03-04 19:30:13 -0500

    refactor D

--- src/a-d/Donchian/Donchian.cs
+++ src/a-d/Donchian/Donchian.cs
@@ -63,19 +63,6 @@ public static partial class Indicator
         return results;
     }
 
-    // remove recommended periods
-    /// <include file='../../_common/Results/info.xml' path='info/type[@name="Prune"]/*' />
-    ///
-    public static IEnumerable<DonchianResult> RemoveWarmupPeriods(
-        this IEnumerable<DonchianResult> results)
-    {
-        int removePeriods = results
-          .ToList()
-          .FindIndex(x => x.Width != null);
-
-        return results.Remove(removePeriods);
-    }
-
     // parameter validation
     private static void ValidateDonchian(
         int lookbackPeriods)
--- src/a-d/DoubleEma/DoubleEma.cs
+++ src/a-d/DoubleEma/DoubleEma.cs
@@ -51,19 +51,6 @@ public static partial class Indicator
         return results;
     }
 
-    // remove recommended periods
-    /// <include file='../../_common/Results/info.xml' path='info/type[@name="Prune"]/*' />
-    ///
-    public static IEnumerable<DemaResult> RemoveWarmupPeriods(
-        this IEnumerable<DemaResult> results)
-    {
-        int n2 = results
-          .ToList()
-          .FindIndex(x => x.Dema != null) + 2;
-
-        return results.Remove(n2 + 100);
-    }
-
     // parameter validation
     private static void ValidateDema(
         int lookbackPeriods)
