41b8028cc3d078040979f49718333821537f5540
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2022-05-14 23:57:15 -0400

    cleanup

--- tests/indicators/_Initialize.cs
+++ tests/indicators/_Initialize.cs
@@ -25,4 +25,5 @@ public abstract class TestBase
     internal static readonly IEnumerable<Quote> mismatchQuotes = TestData.GetMismatch();
     internal static readonly IEnumerable<Quote> noquotes = new List<Quote>();
     internal static readonly IEnumerable<Quote> onequote = TestData.GetDefault(1);
+    internal static readonly IEnumerable<Quote> randomQuotes = TestData.GetRandom(1000);
 }
