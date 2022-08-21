de158842659962d0a71b69ae27798227e4f2ff54
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2022-08-20 21:16:05 -0400

    .ToQuotes() for HeikinAshi (#873)

---
+++ src/e-k/HeikinAshi/HeikinAshi.Utilities.cs
@@ -0,0 +1,20 @@
+namespace Skender.Stock.Indicators;
+
+public static partial class Indicator
+{
+    // convert results to quotes
+    internal static List<Quote> ToQuotes(
+    this IEnumerable<HeikinAshiResult> results)
+      => results
+        .Select(x => new Quote
+        {
+            Date = x.Date,
+            Open = x.Open,
+            High = x.High,
+            Low = x.Low,
+            Close = x.Close,
+            Volume = x.Volume
+        })
+        .OrderBy(x => x.Date)
+        .ToList();
+}
