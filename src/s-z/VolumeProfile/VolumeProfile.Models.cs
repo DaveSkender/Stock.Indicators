bf38e8301c32796ebf4ba3c958f0206d5b17b7e5
JGronholz <jgronholz@live.com>
2022-08-22 16:14:44 -0500

    Volume Profile

---
+++ src/s-z/VolumeProfile/VolumeProfile.Models.cs
@@ -0,0 +1,69 @@
+namespace Skender.Stock.Indicators;
+
+[Serializable]
+public class VpvrResult : ResultBase
+{
+    public VpvrResult(IQuote quote, VpvrResult previousResult)
+    {
+        this.previousResult = previousResult;
+
+        if (quote is null)
+        {
+            throw new ArgumentNullException(nameof(quote));
+        }
+
+        Date = quote.Date;
+        High = quote.High;
+        Low = quote.Low;
+        Volume = quote.Volume;
+    }
+
+    private VpvrResult previousResult;
+
+    public decimal High { get; private set; }
+    public decimal Low { get; private set; }
+    public decimal Volume { get; private set; }
+    public IEnumerable<VpvrValue> VolumeProfile { get; internal set; }
+    public IEnumerable<VpvrValue> CumulativeVolumeProfile
+    {
+        get
+        {
+            List<VpvrValue> vpvrValues = cumulativeVolumeProfile.Select((kvp) => new VpvrValue(kvp.Key, kvp.Value)).ToList();
+            vpvrValues.Sort((first, second) => first.Price.CompareTo(second.Price));
+            return vpvrValues;
+        }
+    }
+    private Dictionary<decimal, decimal> cumulativeVolumeProfile
+    {
+        get
+        {
+            Dictionary<decimal, decimal> vpvrTotals = previousResult?.cumulativeVolumeProfile ?? new Dictionary<decimal, decimal>();
+
+            foreach (VpvrValue item in VolumeProfile)
+            {
+                if (vpvrTotals.ContainsKey(item.Price))
+                {
+                    vpvrTotals[item.Price] += item.Volume;
+                }
+                else
+                {
+                    vpvrTotals.Add(item.Price, item.Volume);
+                }
+            }
+
+            return vpvrTotals;
+        }
+    }
+}
+
+public class VpvrValue
+{
+    public VpvrValue(decimal price, decimal volume)
+    {
+        Price = price;
+        Volume = volume;
+    }
+
+    public decimal Price { get; set; }
+    public decimal Volume { get; set; }
+}
---
+++ src/s-z/VolumeProfile/VolumeProfile.cs
@@ -0,0 +1,62 @@
+namespace Skender.Stock.Indicators;
+
+public static partial class Indicator
+{
+    // Volume Profile
+    /// <include file='./info.xml' path='indicator/*' />
+    ///
+    public static IEnumerable<VpvrResult> GetVpvr(this IEnumerable<IQuote> quotes, decimal precision = 0.001M)
+    {
+        ValidateVpvr(precision);
+
+        List<VpvrResult> results = new List<VpvrResult>();
+        VpvrResult vpvrResult = null;
+        foreach (IQuote quote in quotes)
+        {
+            vpvrResult = new VpvrResult(quote, vpvrResult);
+            results.Add(vpvrResult);
+            vpvrResult.getVolumeProfile(precision);
+        }
+
+        return results;
+    }
+
+    private static IEnumerable<VpvrValue> getVolumeProfile(this VpvrResult vpvrResult, decimal precision = 0.001M)
+    {
+        ValidateVpvr(precision);
+
+        // if ((vpvrResult.VolumeProfile == null) || (vpvrResult.currentPrecision != precision))
+        {
+            decimal high = Math.Ceiling(vpvrResult.High / precision) * precision;
+            decimal low = Math.Floor(vpvrResult.Low / precision) * precision;
+            decimal delta = high - low;
+
+            List<VpvrValue> results = new List<VpvrValue>();
+            if (delta > 0)
+            {
+                decimal intervalCount = delta / precision; // should be even number??
+                decimal volumeSliceSize = vpvrResult.Volume / intervalCount;
+
+                for (decimal price = low; price <= high; price += precision)
+                {
+                    results.Add(new VpvrValue(price, volumeSliceSize));
+                }
+
+                results.Sort((first, second) => first.Price.CompareTo(second.Price));
+            }
+
+            vpvrResult.VolumeProfile = results;
+        }
+
+        return vpvrResult.VolumeProfile;
+    }
+
+    // parameter validation
+    private static void ValidateVpvr(decimal precision)
+    {
+        if (precision <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(precision), precision, "Precision must be greater than 0.");
+        }
+    }
+}
