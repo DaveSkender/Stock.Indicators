139146210ead7f88ba896235075a85a862b458f9
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2022-10-15 18:12:00 -0400

    misc doc

--- src/s-z/StdDevChannels/StdDevChannels.Api.cs
+++ src/s-z/StdDevChannels/StdDevChannels.Api.cs
@@ -9,25 +9,25 @@ public static partial class Indicator
     public static IEnumerable<StdDevChannelsResult> GetStdDevChannels<TQuote>(
         this IEnumerable<TQuote> quotes,
         int? lookbackPeriods = 20,
-        double standardDeviations = 2)
+        double stdDeviations = 2)
         where TQuote : IQuote => quotes
             .ToBasicTuple(CandlePart.Close)
-            .CalcStdDevChannels(lookbackPeriods, standardDeviations);
+            .CalcStdDevChannels(lookbackPeriods, stdDeviations);
 
     // SERIES, from CHAIN
     public static IEnumerable<StdDevChannelsResult> GetStdDevChannels(
         this IEnumerable<IReusableResult> results,
         int? lookbackPeriods = 20,
-        double standardDeviations = 2) => results
+        double stdDeviations = 2) => results
             .ToResultTuple()
-            .CalcStdDevChannels(lookbackPeriods, standardDeviations)
+            .CalcStdDevChannels(lookbackPeriods, stdDeviations)
             .SyncIndex(results, SyncType.Prepend);
 
     // SERIES, from TUPLE
     public static IEnumerable<StdDevChannelsResult> GetStdDevChannels(
         this IEnumerable<(DateTime, double)> priceTuples,
         int? lookbackPeriods = 20,
-        double standardDeviations = 2) => priceTuples
+        double stdDeviations = 2) => priceTuples
             .ToSortedList()
-            .CalcStdDevChannels(lookbackPeriods, standardDeviations);
+            .CalcStdDevChannels(lookbackPeriods, stdDeviations);
 }
