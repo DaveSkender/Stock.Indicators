4aaed57843ed71328b8cdf77787ca904f9bea3d7
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2022-11-26 20:40:45 -0500

    update STARC Bands

--- tests/indicators/s-z/StarcBands/StarcBands.Tests.cs
+++ tests/indicators/s-z/StarcBands/StarcBands.Tests.cs
@@ -18,10 +18,7 @@ public class StarcBands : TestBase
             quotes.GetStarcBands(smaPeriods, multiplier, atrPeriods)
             .ToList();
 
-        // assertions
-
         // proper quantities
-        // should always be the same number of results as there is quotes
         Assert.AreEqual(502, results.Count);
         Assert.AreEqual(483, results.Count(x => x.Centerline != null));
         Assert.AreEqual(483, results.Count(x => x.UpperBand != null));
@@ -33,25 +30,25 @@ public class StarcBands : TestBase
         Assert.AreEqual(null, r1.UpperBand);
         Assert.AreEqual(null, r1.LowerBand);
 
-        StarcBandsResult r2 = results[19];
-        Assert.AreEqual(214.5250, NullMath.Round(r2.Centerline, 4));
-        Assert.AreEqual(217.2831, NullMath.Round(r2.UpperBand, 4));
-        Assert.AreEqual(211.7669, NullMath.Round(r2.LowerBand, 4));
+        StarcBandsResult r19 = results[19];
+        Assert.AreEqual(214.5250, NullMath.Round(r19.Centerline, 4));
+        Assert.AreEqual(217.2345, NullMath.Round(r19.UpperBand, 4));
+        Assert.AreEqual(211.8155, NullMath.Round(r19.LowerBand, 4));
 
-        StarcBandsResult r3 = results[249];
-        Assert.AreEqual(255.5500, NullMath.Round(r3.Centerline, 4));
-        Assert.AreEqual(258.2261, NullMath.Round(r3.UpperBand, 4));
-        Assert.AreEqual(252.8739, NullMath.Round(r3.LowerBand, 4));
+        StarcBandsResult r249 = results[249];
+        Assert.AreEqual(255.5500, NullMath.Round(r249.Centerline, 4));
+        Assert.AreEqual(258.2261, NullMath.Round(r249.UpperBand, 4));
+        Assert.AreEqual(252.8739, NullMath.Round(r249.LowerBand, 4));
 
-        StarcBandsResult r4 = results[485];
-        Assert.AreEqual(265.4855, NullMath.Round(r4.Centerline, 4));
-        Assert.AreEqual(275.1161, NullMath.Round(r4.UpperBand, 4));
-        Assert.AreEqual(255.8549, NullMath.Round(r4.LowerBand, 4));
+        StarcBandsResult r485 = results[485];
+        Assert.AreEqual(265.4855, NullMath.Round(r485.Centerline, 4));
+        Assert.AreEqual(275.1161, NullMath.Round(r485.UpperBand, 4));
+        Assert.AreEqual(255.8549, NullMath.Round(r485.LowerBand, 4));
 
-        StarcBandsResult r5 = results[501];
-        Assert.AreEqual(251.8600, NullMath.Round(r5.Centerline, 4));
-        Assert.AreEqual(264.1595, NullMath.Round(r5.UpperBand, 4));
-        Assert.AreEqual(239.5605, NullMath.Round(r5.LowerBand, 4));
+        StarcBandsResult r501 = results[501];
+        Assert.AreEqual(251.8600, NullMath.Round(r501.Centerline, 4));
+        Assert.AreEqual(264.1595, NullMath.Round(r501.UpperBand, 4));
+        Assert.AreEqual(239.5605, NullMath.Round(r501.LowerBand, 4));
     }
 
     [TestMethod]
