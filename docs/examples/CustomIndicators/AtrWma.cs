414b6c60374907adf087b44d7cbe97a5a1e952de
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2022-12-01 08:07:39 -0500

    undo test in examples project

--- docs/examples/CustomIndicators/AtrWma.cs
+++ docs/examples/CustomIndicators/AtrWma.cs
@@ -4,12 +4,9 @@ namespace Custom.Stock.Indicators;
 
 // Custom results class
 // This inherits many of the extension methods, like .RemoveWarmupPeriods()
-public class AtrWmaResult : ResultBase, IReusableResult
+public class AtrWmaResult : ResultBase
 {
     public double? AtrWma { get; set; }
-    
-    // to enable further chaining
-    double? IReusableResult.Value => AtrWma;
 }
 
 public static class CustomIndicators
