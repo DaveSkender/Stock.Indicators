5359dee43b524e10aee2397bc864858cd1a6edf6
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2022-12-28 12:00:42 -0500

    add xml docs

--- src/a-d/Adl/Adl.Utilities.cs
+++ src/a-d/Adl/Adl.Utilities.cs
@@ -3,16 +3,18 @@ namespace Skender.Stock.Indicators;
 public static partial class Indicator
 {
     // select element
-    /// <include file='../../_common/Results/info.xml' path='info/type[@name="Select"]/*' />
+    /// <include file='../../_common/Results/info.xml' path='info/type[@name="TupleSelect"]/*' />
     ///
     public static IEnumerable<(DateTime Date, double? Value)> ToTupleResult(
         this IEnumerable<AdlResult> results,
-        string element) => results
+        string element)
+        => results
           .Select(r => element switch
           {
-            "MoneyFlowMultiplier" => (r.Date, r.MoneyFlowMultiplier),
-            "MoneyFlowVolume" => (r.Date, r.MoneyFlowVolume),
-            "Adl" => (r.Date, r.Adl),
-            "AdlSma" => (r.Date, r.AdlSma)
+              "MoneyFlowMultiplier" => (r.Date, r.MoneyFlowMultiplier),
+              "MoneyFlowVolume" => (r.Date, r.MoneyFlowVolume),
+              "Adl" => (r.Date, r.Adl),
+              "AdlSma" => (r.Date, r.AdlSma),
+              _ => throw new ArgumentOutOfRangeException(nameof(element), "Unknown element provided."),
           });
 }
