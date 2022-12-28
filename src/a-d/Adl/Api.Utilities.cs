b616d627b0f1ec3b790f99f5e327aa5224b32d3e
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2022-12-27 22:16:48 -0500

    sample selector for Adl

---
+++ src/a-d/Adl/Api.Utilities.cs
@@ -0,0 +1,18 @@
+namespace Skender.Stock.Indicators;
+
+public static partial class Indicator
+{
+    // select element
+    /// <include file='../../_common/Results/info.xml' path='info/type[@name="Select"]/*' />
+    ///
+    public static IEnumerable<(DateTime Date, double? Value> ToTupleCollection(
+        this IEnumerable<AdxResult> results,
+        string element) => results
+          .Select(r => element switch
+          {
+            "MoneyFlowMultiplier" => (r.Date, r.MoneyFlowMultiplier),
+            "MoneyFlowVolume" => (r.Date, r.MoneyFlowVolume),
+            "Adl" => (r.Date, r.Adl),
+            "AdlSma" => (r.Date, r.AdlSma)
+          }
+}
