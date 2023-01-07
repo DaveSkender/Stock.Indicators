47e7f5841bd23758a9620cfa7202452faad37dda
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2023-01-07 18:00:23 -0500

    add FindIndex by date, for series

--- tests/indicators/_common/Generics/Test.Seek.cs
+++ tests/indicators/_common/Generics/Test.Seek.cs
@@ -8,7 +8,7 @@ namespace Tests.Indicators;
 public class Seeking : TestBase
 {
     [TestMethod]
-    public void Find()
+    public void FindSeries()
     {
         IEnumerable<Quote> quotes = TestData.GetDefault();
         IEnumerable<EmaResult> emaResults = quotes.GetEma(20);
@@ -18,5 +18,32 @@ public class Seeking : TestBase
 
         EmaResult r = emaResults.Find(findDate);
         Assert.AreEqual(249.3519, r.Ema.Round(4));
+    }
+
+    [TestMethod]
+    public void FindSeriesNone()
+    {
+        IEnumerable<Quote> quotes = TestData.GetDefault();
+        IEnumerable<EmaResult> emaResults = quotes.GetEma(20);
+
+        // find specific date
+        DateTime findDate = DateTime.ParseExact("1928-10-29", "yyyy-MM-dd", EnglishCulture);
+
+        EmaResult r = emaResults.Find(findDate);
+        Assert.IsNull(r);
+    }
+
+    [TestMethod]
+    public void FindSeriesIndex()
+    {
+        List<Quote> quotes = TestData
+            .GetDefault()
+            .ToSortedList();
+
+        // find specific date
+        DateTime findDate = DateTime.ParseExact("2018-12-31", "yyyy-MM-dd", EnglishCulture);
+
+        int i = quotes.FindIndex(findDate);
+        Assert.AreEqual(501, i);
     }
 }
--- tests/performance/GlobalSuppressions.cs
+++ tests/performance/GlobalSuppressions.cs
@@ -16,10 +16,3 @@ using System.Diagnostics.CodeAnalysis;
     Justification = "Required for BenchmarkDotNet",
     Scope = "member",
     Target = "~F:Tests.Performance.InternalsPerformance.Periods")]
-
-[assembly: SuppressMessage(
-    "StyleCop.CSharp.SpacingRules",
-    "SA1005:Single line comments should begin with single space",
-    Justification = "I like to comment out performance tests not normally used.",
-    Scope = "member",
-    Target = "~M:Tests.Performance.IndicatorPerformance.GetEpma~System.Object")]
