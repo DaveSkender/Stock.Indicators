65a425305d334bb5e2f6c60c1c24bf5305c81606
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2024-01-28 01:43:00 -0500

    XML comment example

--- src/_common/Quotes/Quote.Models.cs
+++ src/_common/Quotes/Quote.Models.cs
@@ -2,28 +2,65 @@ namespace Skender.Stock.Indicators;
 
 // QUOTE MODELS
 
+/// <summary>
+/// Quote interface can be used to modify or create your own quote class
+/// for compatibility to this library.
+/// </summary>
 public interface IQuote : ISeries
 {
-    public decimal Open { get; }
-    public decimal High { get; }
-    public decimal Low { get; }
-    public decimal Close { get; }
-    public decimal Volume { get; }
+    /// <summary>
+    /// Opening ticker price of this bar
+    /// </summary>
+    decimal Open { get; }
+
+    /// <summary>
+    /// High ticker price of this bar
+    /// </summary>
+    decimal High { get; }
+
+    /// <summary>
+    /// Low ticker price of this bar
+    /// </summary>
+    decimal Low { get; }
+
+    /// <summary>
+    /// Last ticker price of this bar
+    /// </summary>
+    decimal Close { get; }
+
+    /// <summary>
+    /// Quantity of units transacted during this bar
+    /// </summary>
+    decimal Volume { get; }
 }
 
-[Serializable]
-public class Quote : IQuote
+/// <summary>
+/// Built-in Quote class can be used for providing quotes to this library.
+/// This is a good choice if you have no current intention of extending it.
+/// </summary>
+public record class Quote : IQuote
 {
+    /// <inheritdoc/>
     public DateTime Date { get; set; }
+
+    /// <inheritdoc/>
     public decimal Open { get; set; }
+
+    /// <inheritdoc/>
     public decimal High { get; set; }
+
+    /// <inheritdoc/>
     public decimal Low { get; set; }
+
+    /// <inheritdoc/>
     public decimal Close { get; set; }
+
+    /// <inheritdoc/>
     public decimal Volume { get; set; }
 }
 
-[Serializable]
-internal class QuoteD
+// internal use only, double variant
+internal record class QuoteD
 {
     internal DateTime Date { get; set; }
     internal double Open { get; set; }
