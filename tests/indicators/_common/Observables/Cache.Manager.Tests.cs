800042b5d146c9a4b66653e6af05007a932a762e
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2024-07-06 17:36:28 -0400

    test cleanup

--- tests/indicators/_common/Observables/Cache.Manager.Tests.cs
+++ tests/indicators/_common/Observables/Cache.Manager.Tests.cs
@@ -4,13 +4,61 @@ namespace Tests.Common.Observables;
 public class CacheMgmtTests : TestBase
 {
     [TestMethod]
-    public void Analysis() => Assert.Inconclusive("test not implemented");
+    public void ModifyWithAnalysis() => Assert.Inconclusive("test not implemented");
+
+    [TestMethod]
+    public void ModifyWithAct() => Assert.Inconclusive("test not implemented");
 
     [TestMethod]
     public void Purge() => Assert.Inconclusive("test not implemented");
 
+    [TestMethod]  // TODO: tests should include all Act enum methods
+    public void ActInstructions() => Assert.Inconclusive("test not implemented");
+
     [TestMethod]
-    public void Modify() => Assert.Inconclusive("test not implemented");
+    public void ActAddOld()  // late arrival
+    {
+        List<Quote> quotesList = Quotes
+            .ToSortedList();
+
+        int length = Quotes.Count();
+
+        // add base quotes
+        QuoteHub<Quote> provider = new();
+
+        Use<Quote> observer = provider
+            .Use(CandlePart.Close);
+
+        // emulate incremental quotes
+        for (int i = 0; i < length; i++)
+        {
+            // skip one
+            if (i == 100)
+            {
+                continue;
+            }
+
+            Quote q = quotesList[i];
+            provider.Add(q);
+        }
+
+        // add late
+        provider.Add(quotesList[100]);
+
+        // assert same as original
+        for (int i = 0; i < length; i++)
+        {
+            Quote q = quotesList[i];
+            Reusable r = observer.StreamCache.Cache[i];
+
+            // compare quote to result cache
+            r.Timestamp.Should().Be(q.Timestamp);
+            r.Value.Should().Be((double)q.Close);
+        }
+
+        // close observations
+        provider.EndTransmission();
+    }
 
     [TestMethod]
     public void Overflowing()
@@ -85,64 +133,4 @@ public class CacheMgmtTests : TestBase
 
         provider.EndTransmission();
     }
-
-    [TestMethod]
-    public void ActAddNew() => Assert.Inconclusive("test not implemented");
-
-    [TestMethod]
-    public void ActAddOld()  // late arrival
-    {
-        List<Quote> quotesList = Quotes
-            .ToSortedList();
-
-        int length = Quotes.Count();
-
-        // add base quotes
-        QuoteHub<Quote> provider = new();
-
-        Use<Quote> observer = provider
-            .Use(CandlePart.Close);
-
-        // emulate incremental quotes
-        for (int i = 0; i < length; i++)
-        {
-            // skip one
-            if (i == 100)
-            {
-                continue;
-            }
-
-            Quote q = quotesList[i];
-            provider.Add(q);
-        }
-
-        // add late
-        provider.Add(quotesList[100]);
-
-        // assert same as original
-        for (int i = 0; i < length; i++)
-        {
-            Quote q = quotesList[i];
-            Reusable r = observer.StreamCache.Cache[i];
-
-            // compare quote to result cache
-            r.Timestamp.Should().Be(q.Timestamp);
-            r.Value.Should().Be((double)q.Close);
-        }
-
-        // close observations
-        provider.EndTransmission();
-    }
-
-    [TestMethod]
-    public void ActUpdate() => Assert.Inconclusive("test not implemented");
-
-    [TestMethod]
-    public void ActDelete() => Assert.Inconclusive("test not implemented");
-
-    [TestMethod]
-    public void ActDoNothing() => Assert.Inconclusive("test not implemented");
-
-    [TestMethod]
-    public void ActAutoPrune() => Assert.Inconclusive("test not implemented");
 }
