e4e956438fe2c50e03d8de8f552e92172654df1a
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2024-07-29 17:19:44 -0400

    add EMA incremental methods

--- tests/indicators/e-k/Ema/Ema.Increments.Tests.cs
+++ tests/indicators/e-k/Ema/Ema.Increments.Tests.cs
@@ -1,17 +1,41 @@
 namespace Increments;
 
 [TestClass]
-public class EmaTests : IncrementsTestBase
+public class Ema : IncrementsTestBase
 {
     [TestMethod]
     public override void Standard()
     {
-        var ema = new Ema(14);
+        EmaList<Quote> sut = new(14);
+
+        foreach (Quote quote in Quotes)
+        {
+            sut.Add(quote);
+        }
+
+        IReadOnlyList<EmaResult> series
+            = Quotes.ToEma(14);
+
+        sut.Should().HaveCount(Quotes.Count);
+        sut.Should().BeEquivalentTo(series);
     }
 
     [TestMethod]
     public override void ValueBased()
     {
-        Assert.Fail("Test not implemented");
+        EmaArray sut = new(14);
+
+        foreach (Quote quote in Quotes)
+        {
+            sut.Add(quote.Value);
+        }
+
+        List<double?> series = Quotes
+            .ToEma(14)
+            .Select(x => x.Ema)
+            .ToList();
+
+        sut.Should().HaveCount(Quotes.Count);
+        sut.Should().BeEquivalentTo(series);
     }
 }
