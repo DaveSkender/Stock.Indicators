bbc233267ebb54ba3bc30c897812b318bee1822e
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2024-07-28 20:59:34 -0400

    docs: fix BasicData internal link refs

--- src/_common/Use (QuotePart)/Use.Api.cs
+++ src/_common/Use (QuotePart)/Use.Api.cs
@@ -5,19 +5,20 @@ namespace Skender.Stock.Indicators;
 public static partial class QuotePartUtility
 {
     // SERIES, from Quotes
-    public static IReadOnlyList<QuotePart> Use(
-        this IEnumerable<IQuote> quotes,
+    public static IReadOnlyList<QuotePart> Use<TQuote>(
+        this IEnumerable<TQuote> quotes,
         CandlePart candlePart)
+        where TQuote : IQuote
         => quotes
             .OrderBy(q => q.Timestamp)
             .Select(q => q.ToQuotePart(candlePart))
             .ToList();
 
-    // SERIES, from Quotes
-    public static IReadOnlyList<QuotePart> Use<TQuote>(
-        this IEnumerable<TQuote> quotes,
+    // SERIES, from Quotes (QuotePart alias)
+    /// <inheritdoc cref="Use{TQuote}(IEnumerable{TQuote}, CandlePart)"/>
+    public static IReadOnlyList<QuotePart> GetQuotePart<TQuote>(
+        this IEnumerable<IQuote> quotes,
         CandlePart candlePart)
-        where TQuote : IQuote
         => quotes
             .OrderBy(q => q.Timestamp)
             .Select(q => q.ToQuotePart(candlePart))
