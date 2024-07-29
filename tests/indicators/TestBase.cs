68de41626ecb680f8fa11c2edeae365794904bdc
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2024-07-29 10:23:57 -0400

    refactor: rename files accordingly

--- tests/indicators/TestBase.cs
+++ tests/indicators/TestBase.cs
@@ -48,17 +48,6 @@ public abstract class StaticSeriesTestBase : TestBase
 }
 
 /// <summary>
-/// Base tests that all static indicators (series) should have.
-/// </summary>
-[TestClass]
-public abstract class IncrementsTestBase : TestBase
-{
-    [TestMethod]
-    public abstract void Standard();
-}
-
-
-/// <summary>
 /// Base tests that all streamed indicators should have.
 /// </summary>
 [TestClass]
