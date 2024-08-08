4e042cd4d6bfd2a149f171a56d0f64cc12ae245c
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2024-08-07 23:09:13 -0400

    add vectorization for static SMA

--- src/s-z/Sma/Sma.StaticSeries.cs
+++ src/s-z/Sma/Sma.StaticSeries.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 namespace Skender.Stock.Indicators;
 
 // SIMPLE MOVING AVERAGE (SERIES)
@@ -15,6 +17,12 @@ public static partial class Sma
         // initialize
         int length = source.Count;
         SmaResult[] results = new SmaResult[length];
+        double[] values = new double[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            values[i] = source[i].Value;
+        }
 
         // roll through source values
         for (int i = 0; i < length; i++)
@@ -26,10 +34,22 @@ public static partial class Sma
             if (i >= lookbackPeriods - 1)
             {
                 double sumSma = 0;
-                for (int p = i - lookbackPeriods + 1; p <= i; p++)
+                int start = i - lookbackPeriods + 1;
+                int end = i + 1;
+
+                // SIMD optimization
+                int vectorSize = Vector<double>.Count;
+                int simdEnd = start + ((end - start) / vectorSize * vectorSize);
+
+                for (int p = start; p < simdEnd; p += vectorSize)
                 {
-                    T ps = source[p];
-                    sumSma += ps.Value;
+                    Vector<double> vector = new(values, p);
+                    sumSma += Vector.Sum(vector);
+                }
+
+                for (int p = simdEnd; p < end; p++)
+                {
+                    sumSma += values[p];
                 }
 
                 sma = sumSma / lookbackPeriods;
