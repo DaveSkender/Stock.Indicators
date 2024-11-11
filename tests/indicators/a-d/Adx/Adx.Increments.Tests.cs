a6ea3d46552c552846113a31362f2b64fb9fd7eb
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2024-11-11 17:04:02 -0500

    add ADX buffered list

--- tests/indicators/a-d/Adx/Adx.Increments.Tests.cs
+++ tests/indicators/a-d/Adx/Adx.Increments.Tests.cs
@@ -1,51 +1,12 @@
 namespace Increments;
 
 [TestClass]
-public class Adx : IncrementsTestBase
+public class Adx : BufferListTestBase
 {
     private const int lookbackPeriods = 14;
 
-    private static readonly IReadOnlyList<IReusable> reusables
-       = Quotes
-        .Cast<IReusable>()
-        .ToList();
-
     private static readonly IReadOnlyList<AdxResult> series
        = Quotes.ToAdx(lookbackPeriods);
-
-    [TestMethod]
-    public void FromReusableSplit()
-    {
-        AdxList sut = new(lookbackPeriods);
-
-        foreach (IReusable item in reusables)
-        {
-            sut.Add(item.Timestamp, item.Value);
-        }
-
-        sut.Should().HaveCount(Quotes.Count);
-        sut.Should().BeEquivalentTo(series);
-    }
-
-    [TestMethod]
-    public void FromReusableItem()
-    {
-        AdxList sut = new(lookbackPeriods);
-
-        foreach (IReusable item in reusables) { sut.Add(item); }
-
-        sut.Should().HaveCount(Quotes.Count);
-        sut.Should().BeEquivalentTo(series);
-    }
-
-    [TestMethod]
-    public void FromReusableBatch()
-    {
-        AdxList sut = new(lookbackPeriods) { reusables };
-
-        sut.Should().HaveCount(Quotes.Count);
-        sut.Should().BeEquivalentTo(series);
-    }
 
     [TestMethod]
     public override void FromQuote()
