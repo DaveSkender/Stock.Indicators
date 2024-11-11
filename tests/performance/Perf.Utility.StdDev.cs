531ae8ceb3da0051413679708efdbc6b1fef58db
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2024-11-11 10:18:35 -0500

    refactor: Optimize NullMath utilities (#1272)

--- tests/performance/Perf.Utility.StdDev.cs
+++ tests/performance/Perf.Utility.StdDev.cs
@@ -1,22 +1,22 @@
 namespace Tests.Performance;
 
-// INTERNAL FUNCTIONS
+// INTERNAL UTILITIES
 
 [ShortRunJob]
-public class InternalsPerformance
+public class UtilityStdDev
 {
     [Params(20, 50, 250, 1000)]
     public int Periods;
 
-    private double[] values;
+    private double[] _values;
 
     // standard deviation
     [GlobalSetup(Targets = [nameof(StdDev)])]
     public void Setup()
-        => values = TestData.GetLongish(Periods)
+        => _values = TestData.GetLongish(Periods)
             .Select(x => (double)x.Close)
             .ToArray();
 
     [Benchmark]
-    public object StdDev() => values.StdDev();
+    public object StdDev() => _values.StdDev();
 }
