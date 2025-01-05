49d6ddddd98f7f9cadf3af5027bdd22cdfb46f6b
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2025-01-05 17:38:45 -0500

    revert: Restore .NET Standard 2.0 (#1312)
    We'd prematurely removed .NET Standard 2.0
    We do plan on removing it for version 3 since it will limit use of newer SDK features with better overall performance.

--- tests/indicators/_Initialize.cs
+++ tests/indicators/_Initialize.cs
@@ -1,5 +1,7 @@
 using System.Globalization;
+using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
 
 // GLOBALS & INITIALIZATION OF TEST DATA
 
@@ -8,6 +10,9 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Tests.Performance")]
 namespace Tests.Common;
 
+/// <summary>
+/// Base class for all test classes, providing common test data and utilities.
+/// </summary>
 [TestClass]
 public abstract class TestBase
 {
@@ -26,4 +31,38 @@ public abstract class TestBase
     internal static readonly IEnumerable<Quote> randomQuotes = TestData.GetRandom(1000);
     internal static readonly IEnumerable<Quote> zeroesQuotes = TestData.GetZeros();
     internal static readonly IEnumerable<(DateTime, double)> tupleNanny = TestData.GetTupleNaN();
+}
+
+/// <summary>
+/// Test class for the startup of the test project.
+/// </summary>
+[TestClass]
+public class Startup
+{
+    /// <summary>
+    /// Displays the assembly location, name, version, and target framework
+    /// as a sanity check for test runner targeting.
+    /// </summary>
+    [TestMethod]
+    [TestCategory("ShowMe")]
+    public void ShowFramework()
+    {
+        // Get the assembly of any type from your Indicators project
+        Assembly assembly = typeof(Indicator).Assembly;
+
+        Console.WriteLine($"Assembly Location: {assembly.Location}");
+        Console.WriteLine($"Assembly Name: {assembly.GetName().Name}");
+        Console.WriteLine($"Assembly Version: {assembly.GetName().Version}");
+
+        // Get the target framework the assembly was built for
+        TargetFrameworkAttribute targetFrameworkAttribute = assembly
+            .GetCustomAttribute<TargetFrameworkAttribute>();
+
+        string frameworkName
+            = targetFrameworkAttribute?.FrameworkName ?? "Unknown";
+
+        Console.WriteLine($"Target Framework: {frameworkName}");
+
+        frameworkName.Should().NotBe("Unknown");
+    }
 }
