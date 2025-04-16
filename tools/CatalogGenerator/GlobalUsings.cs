3888259f94a726d14e5801e6d57b454413861b61
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2025-04-16 02:07:41 -0400

    Remove unused Skender.Stock.Indicators global using

--- tools/CatalogGenerator/GlobalUsings.cs
+++ tools/CatalogGenerator/GlobalUsings.cs
@@ -4,4 +4,3 @@ global using System.Text;
 global using Microsoft.CodeAnalysis;
 global using Microsoft.CodeAnalysis.CSharp;
 global using Microsoft.CodeAnalysis.CSharp.Syntax;
-global using Skender.Stock.Indicators;
