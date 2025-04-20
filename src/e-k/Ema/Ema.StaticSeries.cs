792dcbd05a7cb10561ac704888341adef0c79f22
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2025-04-20 17:44:03 -0400

    revert ema changes

--- src/e-k/Ema/Ema.StaticSeries.cs
+++ src/e-k/Ema/Ema.StaticSeries.cs
@@ -62,19 +62,4 @@ public static partial class Ema
 
         return new List<EmaResult>(results);
     }
-
-    /// <summary>
-    /// Converts a list of quotes to EMA results.
-    /// </summary>
-    /// <param name="quotes">The list of quotes.</param>
-    /// <param name="lookbackPeriods">The number of periods to look back for the calculation.</param>
-    /// <returns>A list of EMA results.</returns>
-    public static IReadOnlyList<EmaResult> ToEma(
-        this IReadOnlyList<IQuote> quotes,
-        int lookbackPeriods)
-    {
-        return quotes
-            .ToQuoteDList()
-            .ToEma(lookbackPeriods);
-    }
 }
