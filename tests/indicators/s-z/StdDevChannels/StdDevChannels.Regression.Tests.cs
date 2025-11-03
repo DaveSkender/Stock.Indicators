44c1bd0277ca1e794ea1b9eb9aa0f9a51f82fd33
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2025-11-03 03:11:24 -0500

    Remove inconclusive tests for Buffer and Stream
    Removed inconclusive tests for Buffer and Stream methods.
    
    Signed-off-by: Dave Skender <8432125+DaveSkender@users.noreply.github.com>

--- tests/indicators/s-z/StdDevChannels/StdDevChannels.Regression.Tests.cs
+++ tests/indicators/s-z/StdDevChannels/StdDevChannels.Regression.Tests.cs
@@ -7,10 +7,4 @@ public class StddevchannelsTests : RegressionTestBase<StdDevChannelsResult>
 
     [TestMethod]
     public override void Series() => Quotes.ToStdDevChannels(20).AssertEquals(Expected);
-
-    [TestMethod]
-    public override void Buffer() => Assert.Inconclusive("Buffer implementation not available due to repaint-by-design algorithm");
-
-    [TestMethod]
-    public override void Stream() => Assert.Inconclusive("Stream implementation not available due to repaint-by-design algorithm");
 }
