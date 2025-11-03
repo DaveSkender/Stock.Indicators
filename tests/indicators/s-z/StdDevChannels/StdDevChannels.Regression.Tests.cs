fe0b226a80f8d0e0c397aa12870bb031e223d611
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2025-11-03 03:19:29 -0500

    test(stddev-channels): add explicit Buffer and Stream overrides throwing NotImplementedException in regression test

--- tests/indicators/s-z/StdDevChannels/StdDevChannels.Regression.Tests.cs
+++ tests/indicators/s-z/StdDevChannels/StdDevChannels.Regression.Tests.cs
@@ -7,4 +7,7 @@ public class StddevchannelsTests : RegressionTestBase<StdDevChannelsResult>
 
     [TestMethod]
     public override void Series() => Quotes.ToStdDevChannels(20).AssertEquals(Expected);
+
+    public override void Buffer() => throw new NotImplementedException("Intentionally not implemented");
+    public override void Stream() => throw new NotImplementedException("Intentionally not implemented");
 }
