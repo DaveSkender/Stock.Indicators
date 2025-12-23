3cb72d20c28657c6a33547f357cc533a489d8ea8
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2025-12-22 20:55:02 -0500

    Remove precision tolerance comments from Bop and Chaikin tests

--- tests/indicators/a-d/Bop/Bop.StreamHub.Tests.cs
+++ tests/indicators/a-d/Bop/Bop.StreamHub.Tests.cs
@@ -56,7 +56,7 @@ public class BopHubTests : StreamHubTestBase, ITestQuoteObserver, ITestChainProv
         // time-series, for comparison
         IReadOnlyList<BopResult> seriesList = quotesList.ToBop(14);
 
-        // assert, should equal series (with tolerance for floating point precision)
+        // assert
         streamList.Should().HaveCount(length - 1);
         streamList.IsExactly(seriesList);
 
@@ -97,7 +97,7 @@ public class BopHubTests : StreamHubTestBase, ITestQuoteObserver, ITestChainProv
            = quotesList
             .ToBop(smoothPeriods);
 
-        // assert, should equal series (with tolerance for floating point precision)
+        // assert
         streamList.Should().HaveCount(length);
         streamList.IsExactly(seriesList);
 
@@ -139,7 +139,7 @@ public class BopHubTests : StreamHubTestBase, ITestQuoteObserver, ITestChainProv
             .ToBop(smoothPeriods)
             .ToEma(emaPeriods);
 
-        // assert, should equal series (with tolerance for floating point precision)
+        // assert
         streamList.Should().HaveCount(length);
         streamList.IsExactly(seriesList);
 
