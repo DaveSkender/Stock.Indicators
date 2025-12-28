9fec4563f96ee7efb2fd5d0463e13d32491796b1
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2025-12-28 02:17:17 -0500

    remove extra copy in SMA, add tests

--- src/s-z/Sma/Sma.Utilities.cs
+++ src/s-z/Sma/Sma.Utilities.cs
@@ -1,6 +1,3 @@
-using System.Diagnostics.CodeAnalysis;
-using System.Numerics;
-
 namespace Skender.Stock.Indicators;
 
 /// <summary>
@@ -14,20 +11,18 @@ public static partial class Sma
     /// <param name="values">List of chainable values.</param>
     /// <param name="lookbackPeriods">Window to evaluate, prior to 'endIndex'.</param>
     /// <param name="endIndex">Index position to evaluate or last position when <see langword="null"/>.</param>
-    /// <typeparam name="T">IReusable (chainable) type.</typeparam>
     /// <returns>Simple moving average or <see langword="null"/> if incalculable <see langword="double.NaN"/> values are in range.</returns>
-    public static double? Average<T>(  // public API only
-        this IReadOnlyList<T> values,
+    public static double? Average(  // public API only
+        this IReadOnlyList<IReusable> values,
         int lookbackPeriods,
         int? endIndex = null)
-        where T : IReusable
     {
         ArgumentNullException.ThrowIfNull(values);
 
         return Increment(
             values,
             lookbackPeriods,
-            endIndex ?? values.Count - 1)
+            endIndex ?? (values.Count - 1))
            .NaN2Null();
     }
 
@@ -57,77 +52,6 @@ public static partial class Sma
         return sum / lookbackPeriods;
 
         // TODO: apply this SMA increment method more widely in other indicators (see EMA example)
-    }
-
-    /// <summary>
-    /// Simple moving average calculation.
-    /// </summary>
-    /// <param name="source">List of chainable values.</param>
-    /// <param name="lookbackPeriods">Window to evaluate, prior to 'endIndex'.</param>
-    /// <param name="endIndex">Index position to evaluate.</param>
-    /// <typeparam name="T">IReusable (chainable) type.</typeparam>
-    /// <returns>Simple moving average or <see langword="double.NaN"/> when incalculable.</returns>
-    internal static double Increment<T>(
-        IReadOnlyList<T> source,
-        int lookbackPeriods,
-        int endIndex)
-        where T : IReusable
-    {
-        if (endIndex < lookbackPeriods - 1 || endIndex >= source.Count)
-        {
-            return double.NaN;
-        }
-
-        double sum = 0;
-        for (int i = endIndex - lookbackPeriods + 1; i <= endIndex; i++)
-        {
-            sum += source[i].Value;
-        }
-
-        return sum / lookbackPeriods;
-
-        // TODO: apply this SMA increment method more widely in other indicators (see EMA example)
-    }
-
-    /// <summary>
-    /// Simple moving average calculation using SIMD.
-    /// </summary>
-    /// <param name="prices">Array of prices.</param>
-    /// <param name="period">Period to evaluate.</param>
-    /// <returns>Array of simple moving averages.</returns>
-    /// <remarks>Caution: this experimental method has rounding errors.</remarks>
-    [ExcludeFromCodeCoverage]  // experimental SIMD code
-    internal static double[] Increment(this double[] prices, int period)
-    {
-        // TODO: remove/consider experiment, has rounding errors
-
-        int count = prices.Length - period + 1;
-        double[] sma = new double[count];
-
-        int simdWidth = Vector<double>.Count;
-        for (int i = 0; i < count; i++)
-        {
-            Vector<double> sumVector = Vector<double>.Zero;
-
-            int j;
-            for (j = 0; j <= period - simdWidth; j += simdWidth)
-            {
-                Vector<double> priceVector = new(prices, i + j);
-                sumVector += priceVector;
-            }
-
-            double sum = 0;
-            for (; j < period; j++) // remainder loop
-            {
-                sum += prices[i + j];
-            }
-
-            sum += Vector.Dot(sumVector, Vector<double>.One);
-
-            sma[i] = sum / period;
-        }
-
-        return sma;
     }
 
     /// <summary>
--- tests/indicators/_base/StaticSeriesTestBase.cs
+++ tests/indicators/_base/StaticSeriesTestBase.cs
@@ -20,25 +20,18 @@ public abstract class StaticSeriesTestBase : TestBase
     /// </summary>
     public abstract void NoQuotes_ReturnsEmpty();
 
+    /* MONEY CONSTANTS
+     * Precision constants for BeApproximately() assertions for manually calculated values only.
+     * Maps to equivalent .Round() precision expectations for Series-style indicators only.
+     *
+     * IMPORTANT: DO NOT USE this to address floating point precision issues.  */
 
-    /// <summary>
-    /// Precision constants for BeApproximately() assertions for manually calculated values only.
-    /// Maps to equivalent .Round() precision expectations for Series-style indicators only.
-    /// </summary>
-    /// <remarks>
-    /// IMPORTANT: DO NOT USE this to address floating point precision issues.
-    /// </remarks>
-    protected const double Money3 = 0.0005;    // 3 decimal places: ±0.0005 (equivalent to .Round(3))
-    /// <summary>
-    /// 4 decimal places: &#177;0.00005 (equivalent to .Round(4))
-    /// </summary>
+    // 3 decimal places: ±0.0005 (equivalent to .Round(3))
+    protected const double Money3 = 0.0005;
     protected const double Money4 = 0.00005;
-    /// <summary>
-    /// 5 decimal places: &#177;0.000005 (equivalent to .Round(5))
-    /// </summary>
     protected const double Money5 = 0.000005;
-    /// <summary>
-    /// 6 decimal places: &#177;0.0000005 (equivalent to .Round(6))
-    /// </summary>
     protected const double Money6 = 0.0000005;
+    protected const double Money8 = 0.000000005;
+    protected const double Money10 = 0.00000000005;
+    protected const double Money12 = 0.0000000000005;
 }
