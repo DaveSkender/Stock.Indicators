eb668cb8bec009d8904ce6613ca5cf273dfaaf61
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2025-12-31 13:33:10 -0500

    fix: restore missing RenkoAtr regression tests
    Signed-off-by: Dave Skender <8432125+DaveSkender@users.noreply.github.com>

--- tests/indicators/m-r/RenkoAtr/RenkoAtr.Regression.Tests.cs
+++ tests/indicators/m-r/RenkoAtr/RenkoAtr.Regression.Tests.cs
@@ -1,13 +1,16 @@
 namespace Regression;
 
 [TestClass, TestCategory("Regression")]
-public class RenkoatrTests : RegressionTestBase<RenkoResult>
+public class RenkoAtrTests : RegressionTestBase<RenkoResult>
 {
-    public RenkoatrTests() : base("renko-atr.standard.json") { }
+    public RenkoAtrTests() : base("renko-atr.standard.json") { }
 
     [TestMethod]
     public override void Series() => Quotes.ToRenkoAtr().IsExactly(Expected);
 
-    public override void Buffer() => throw new NotImplementedException("Intentionally not implemented");
-    public override void Stream() => throw new NotImplementedException("Intentionally not implemented");
+    [TestMethod]
+    public override void Buffer() => Assert.Inconclusive("Intentionally not implemented");
+
+    [TestMethod]
+    public override void Stream() => Assert.Inconclusive("Intentionally not implemented");
 }
