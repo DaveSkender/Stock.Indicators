c9ce8e78e6e23a5525acec500534b09909edee96
JGronholz <jgronholz@live.com>
2026-01-02 20:06:57 -0600

    asdf

---
+++ src/_common/StreamHub/IHubState.cs
@@ -0,0 +1,9 @@
+namespace Skender.Stock.Indicators;
+#pragma warning disable IDE0010 // Missing cases in switch expression
+#pragma warning disable CA1040 // Avoid empty interfaces
+
+// STREAM HUB (BASE/CACHE)
+/// <summary>
+/// Marker interface for hub state objects.
+/// </summary>
+public interface IHubState { }
--- src/_common/StreamHub/StreamHub.cs
+++ src/_common/StreamHub/StreamHub.cs
@@ -1,12 +1,4 @@
 namespace Skender.Stock.Indicators;
-#pragma warning disable IDE0010 // Missing cases in switch expression
-#pragma warning disable CA1040 // Avoid empty interfaces
-
-// STREAM HUB (BASE/CACHE)
-/// <summary>
-/// Marker interface for hub state objects.
-/// </summary>
-public interface IHubState { }
 
 /// <inheritdoc cref="IStreamHub{TIn, TOut}"/>
 public abstract partial class StreamHub<TIn, TOut> : IStreamHub<TIn, TOut>
