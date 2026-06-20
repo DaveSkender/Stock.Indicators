e451ee5314626ba4d95595e42fc3927514b1e3c2
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2026-06-20 15:54:34 -0400

    temp: remove exempt HTTP status codes from link checkers

--- docs/.vitepress/test-links.sh
+++ docs/.vitepress/test-links.sh
@@ -15,8 +15,7 @@ if command -v docker >/dev/null 2>&1; then
        /usr/local/bundle/bin/htmlproofer /site \
          --no-enforce-https \
          --no-check-external-hash \
-         --ignore-status-codes '0,302,403,406,408,429,503,999' \
-         --ignore-urls '/fonts.gstatic.com/'"
+         --ignore-urls "fonts.gstatic.com,github.com/DaveSkender/Stock.Indicators"
 else
   echo "Error: Docker is required for htmlproofer"
   exit 1
