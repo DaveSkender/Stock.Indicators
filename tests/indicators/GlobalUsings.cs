385a6a69d359ad691c81e13a0835c1b96715a93b
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2026-06-22 00:32:27 -0400

    docs: remove duplicate global using for FacioQuo.Stock.Indicators in GlobalUsings.cs files

--- tests/indicators/GlobalUsings.cs
+++ tests/indicators/GlobalUsings.cs
@@ -1,7 +1,7 @@
 global using System.Diagnostics;
+global using FacioQuo.Stock.Indicators;
 global using FluentAssertions;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using FacioQuo.Stock.Indicators;
 global using Test.Base;
 global using Test.Data;
 global using Test.Tools;
